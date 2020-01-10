14700e67e686cb36df2e603424dac03bb68f1b20
Luca <sushi2all@gmail.com>
2020-01-10 10:52:58 +0100

    Add missing _key, fulltext code search and limit to product list api

--- backend/Services/modules/product.py
+++ backend/Services/modules/product.py
@@ -1,22 +1,41 @@
+from typing import List, Optional
 from fastapi import APIRouter
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from .db import db
 
 router = APIRouter()
 
 
 class Product(BaseModel):
+  """ 
+  Represents the subset of the Product DB schema necessary 
+  for use in the product list view
+  """
+
+  # the _key field must be aliased because pydantic will not 
+  # accept fields with leading underscore
+  key: str = Field(..., alias="_key")
   code: str
-  description: str
+  description: Optional[str] = None
+  active: bool = True
 
 
 
 @router.get("/")
-async def get_product_list(max: int = 0, search: str = None):
-  limit = max if max else None
-  list = db.collection('Product').all(limit=limit)
-  # search/filtering termporarily implemented only at the client level
-  return [product for product in list]
+async def get_product_list(
+  limit: int = None, # return a limited number of results
+  code: str = None, # filter by code
+):
 
+  list =  db.aql.execute("""
+    LET search = CONCAT('%', @code, '%')
+    FOR p IN Product
+      FILTER !p.trash && LIKE(p.code, search, true)
+      LIMIT @limit
+      RETURN p
+    """, bind_vars={"code": code, "limit": limit})
+
+  results = [Product(**p) for p in list]
+  return results
 
 
