f05d0d3166a39135e5f32dda1591aef59295c520
sushi2all <sushi2all@gmail.com>
2020-05-11 14:03:49 +0200

    Make backend work from any IP in the local network

--- backend/Services/main.py
+++ backend/Services/main.py
@@ -7,14 +7,14 @@ from modules import product, process, bom, item, user, production, org, traceabi
 
 app = FastAPI()
 
-origins = [
-  "http://127.0.0.1:8080",
-  "http://localhost:8080",
-]
+# origins = [
+#   "http://127.0.0.1:8080",
+#   "http://localhost:8080",
+# ]
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=origins,
+  allow_origins=["*"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
