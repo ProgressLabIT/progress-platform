94fdffd94fe5a23c3a970027d42548347a56cc49
sushi2all <sushi2all@gmail.com>
2020-05-11 14:16:02 +0200

    Fix bug in ArangoClient encoder
    jsonable_encoder was not using field aliases

--- backend/Services/utils/db.py
+++ backend/Services/utils/db.py
@@ -4,7 +4,7 @@ from arango import ArangoClient
 from fastapi.encoders import jsonable_encoder
 
 def encoder(data):
-  prepped_data = jsonable_encoder(data)
+  prepped_data = jsonable_encoder(data, by_alias=True)
 
   # remove null _id / _key fields
   for field in ['_id', '_key']:
