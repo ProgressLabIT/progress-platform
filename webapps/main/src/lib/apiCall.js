3adc7360387af45e6fe32c879ac86c6d64e2b2c6
Luca Sorgiacomo <luca@progresslab.it>
2020-12-24 22:00:19 +0100

    Pass api base url to webapp as environment variable

--- webapps/main/src/lib/apiCall.js
+++ webapps/main/src/lib/apiCall.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 import store from '@/store/index.js'
 
-const api_url = process.env.NODE_ENV === 'production'
-	? 'https://' + window.location.hostname + '/api/v1/'
-	: 'http://' + window.location.hostname + '/api/'
+// const api_url = process.env.NODE_ENV === 'production'
+// 	? 'https://' + window.location.hostname + '/api/v1/'
+// 	: 'http://' + window.location.hostname + ':80'
 
+const api_url = process.env.VUE_APP_API_BASE_URL
 
 const api = axios.create({  
-  baseURL: api_url,
+  baseURL: api_url
 })
 
 api.interceptors.request.use( config => {
