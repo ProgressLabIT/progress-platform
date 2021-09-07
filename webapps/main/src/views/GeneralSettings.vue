2199a3ac2b9ab50cc658b14e3afc4c37245d2f37
Luca Sorgiacomo <luca@progresslab.it>
2021-09-07 13:58:58 +0200

    Merge WIP from Bugfix branch

---
+++ webapps/main/src/views/GeneralSettings.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card class="fill">
+
+    <LoadingSignal v-if="!ready"></LoadingSignal>
+
+      <v-row v-else class="fill-height">
+
+        <v-col cols="2" class="fill-height d-flex flex-column">
+
+           <v-tabs vertical background-color="transparent" color="white">
+
+            <v-tab
+              v-for="tab in sections"
+              :to="{ name: tab }"
+              :key="tab"
+              class="menu display mb-2">
+              {{ $tc(`views.${tab}`) }}
+            </v-tab>
+
+          </v-tabs>
+        </v-col>
+
+        <v-divider vertical></v-divider>
+
+        <v-col>
+          <router-view></router-view>
+        </v-col>
+      </v-row>
+  </v-card>
+</template>
+
+<script>
+import LoadingSignal from "@/components/LoadingSignal.vue"
+
+export default {
+
+  name: 'GeneralSettings',
+
+  components: {
+    LoadingSignal
+  },
+
+  data () {
+    return {
+      ready: true,
+      sections: [
+        'companyDetails',
+        'defaultPhaseParams'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.v-tab.menu {
+  justify-content: flex-start;
+}
+</style>
