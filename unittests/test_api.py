2aa1ba17edf240e73c7fb067049c58f95ad147ce
Giuseppe Inchingolo <giu.inch@gmail.com>
2022-01-19 22:16:04 +0100

    Added test cases for the item endpoint, correct status message is returned based on database availability

--- unittests/test_api.py
+++ unittests/test_api.py
@@ -1,15 +1,21 @@
 import pytest
 from pytest_mock import mocker
+from mock import patch
 import os
 import sys
+import time
 
 os.chdir('../backend/api') # all the modules here tested refer to the backend.api
 sys.path.insert(0, '') # more info here: https://stackoverflow.com/questions/57870498/cannot-find-module-after-change-directory
 
-#import utils
-from models.traceability import ProductionEvent
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
+#import utils
+from utils.config import get_config
+from models.traceability import ProductionEvent
 from utils.event import Event
+import endpoints
 
 def test_ProductionEvent():
     '''
@@ -79,7 +85,55 @@ def test_Event():
 
     e = Event(ProductionEvent(event_type='JOB_STARTED')) # iii.
     assert e.db.name == "PROGRESS_TEST"
+    del e
 
+    e = Event(ProductionEvent(event_type='JOB_STARTED', job_key = 12092961, phase_key = 11976627, 
+                                work_order_key = 12092959, user_session_key = 12006040, user_key = 11681276,
+                                work_session_key = 12093618, product_key = 11728033))
+    
+    # TODO: understand why can not execute the save()
+    #ws = e.save()
+    #print(repr(ws))
 
+#@patch("endpoints.item.db")
+def test_router_item(mocker):
+    '''
+    Test for the item endpoint (API router) object, verifies that:
+    1. the status message is correctly associated with the database connection availability (arango container start/stop)
+         a. when the arango container is running, a call to the /item returns a 200 status code
+         b. when the arango container is not running, a call to the /item returns a 500 status code
+    '''
 
+    #with monkeypatch.context() as m:
+    #    m.setattr("endpoints.item", "db", [1,2,3,4])
+
+    #my_db = mocker.patch()
+    #my_db.collection.return_value = [1,2,3,4]
+    #my_db.aql.execute.return_value = [1,2,3,4,5]
+
+    #my_db = mocker.patch('endpoints.item.db', 'collection')
+
+    config = get_config()
+
+    app = FastAPI(
+        # openapi_url=f"{config.root_path}/openapi.json",
+        root_path=config.api_root_path
+    )
+
+    app.include_router(endpoints.item, tags=['Library'])
+
+    client = TestClient(app)
+
+    # 1.
+    os.system('docker start arango')
+    time.sleep(0.1)
+
+    response = client.get("/item")
+    assert response.status_code == 200 # a.
+
+    os.system('docker stop arango')
+    time.sleep(0.1)
+
+    response = client.get("/item")
+    assert response.status_code == 500 # b.
 
