3108c195096fa52a2d3e005936e919666c2a35bc
Luca Sorgiacomo <luca@progresslab.it>
2022-05-31 09:22:50 +0200

    Fix bug in retrieving last user session in UserList query

--- backend/api/utils/org.py
+++ backend/api/utils/org.py
@@ -5,6 +5,6 @@ class Queries:
       FILTER LIKE(TO_STRING(u.active), active_filter) && !u.trash
       SORT u.surname, u.name
       LET dep_data = DOCUMENT(Department, u.department_key)
-      LET last_login = DOCUMENT(u.last_user_session).start
+      LET last_login = DOCUMENT(UserSession, u.last_user_session).start
       RETURN MERGE(u, { department: dep_data }, { last_login })
   """
