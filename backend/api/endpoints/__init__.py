f83092ca7708ad0796c12177b015b9c6c5258e77
Luca Sorgiacomo <luca@progresslab.it>
2022-09-07 15:21:47 +0200

    Add reset production and traceability data as endpoint

--- backend/api/endpoints/__init__.py
+++ backend/api/endpoints/__init__.py
@@ -6,3 +6,4 @@ from .process import router as process
 from .product import router as product
 from .production import router as production
 from .traceability import router as traceability
+from .admin import router as admin
