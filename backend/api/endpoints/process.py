ProgressLabIT/progress-platform

commit b7acc367edc6fdfb7ad980bca7ba967e57d003b8

Prevent BoM loops
Closes #201

backend/api/endpoints/process.py
@@ -193,8 +193,11 @@ async def update_process(process: List[PhaseUpdate], product_key):
       # will set it automatically
       new_phase = True if phase.key == None else False
 
-      if new_phase: exclude_set.add('key')
+      if new_phase:
+        exclude_set.add('key')
+
       prepped_phase_data = jsonable_encoder(phase, by_alias=True, exclude=exclude_set)
+      prepped_phase_data['product_key'] = product_key
       
       phase_update = tx_db.insert_document('Phase', 
         prepped_phase_data, return_new=True, overwrite=True )

backend/api/models/process.py
@@ -83,7 +83,8 @@ class PhaseData(FlexModel):
   key: str = Field(None, alias="_key")
   alias: str
   description: str = None
-  operation_key: str = None
+  product_key: str
+  operation_key: str
   operation_name: str = None
   steps: List[Step] = []
   params: PhaseParameters = PhaseParameters()

backend/api/endpoints/bom.py
@@ -16,9 +16,6 @@ from utils.api import APIResponse
 
 router = APIRouter()
 
-
-
-
 @router.get("/{product_key}/bom")
 async def get_product_bom(product_key):
   try: 
@@ -39,6 +36,7 @@ async def get_product_bom(product_key):
     )
 
 
+
 @router.put('/{product_key}/bom')
 async def update_bom(product_key: str, new_bom: List[BomLineWriteIn]):
   """
@@ -47,12 +45,12 @@ async def update_bom(product_key: str, new_bom: List[BomLineWriteIn]):
   """
 
   # Begin transaction
-  txn = db.begin_transaction(write="requires")
+  tx = db.begin_transaction(write="requires")
   
   try:
 
     # Remove old bom
-    deleted_items = txn.aql.execute(
+    deleted_items = tx.aql.execute(
       Queries.DELETE_PRODUCT_BOM, 
       bind_vars=dict(product_key=product_key)
     )
@@ -60,25 +58,42 @@ async def update_bom(product_key: str, new_bom: List[BomLineWriteIn]):
     bom_to_db = [define_bom_line_for_db(line) for line in new_bom]
 
     # Insert new bom
-    txn.collection('requires').insert_many(bom_to_db, silent=True)    
-    txn.commit_transaction()
-    
-    return APIResponse(message="BoM updated correctly")
+    tx.collection('requires').insert_many(bom_to_db, silent=True)
 
   except Exception as e:
     error_str = traceback.format_exc()
     status_code = 500
-
     response=dict(
       status=status_code,
       message="There was a problem updating the bom. Transaction has been aborted.",
-      error=error_str 
+      error=error_str
     )
-    print("Error! Aborting transaction...")
-    txn.abort_transaction()
+    tx.abort_transaction()
     raise HTTPException(
       status_code=status_code,
       detail=response
     )
+
+  # Check for loops in BoM relationships
+  bom_loops = find_bom_loops(db=tx, product_key=product_key)
+
+  if not len(bom_loops):
+    tx.commit_transaction()
+    return APIResponse(message="BoM updated correctly")
+
+  else:
+    tx.abort_transaction()
+    status_code = 403
+    response=dict(
+      status = status_code,
+      message = "Bom contains loops: at least one of the component requires the current product to be built.",
+      data = bom_loops
+    )
+    raise HTTPException(
+      status_code = status_code,
+      detail = response
+    )
+
+
 
 

backend/api/utils/bom.py
@@ -26,6 +26,21 @@ class Queries:
     REMOVE e IN requires
   """
 
+  CHECK_BOM_LOOP = """
+    LET start = CONCAT('Product/', @product_key)
+    FOR v, e, p in 1..30 OUTBOUND start requires
+    // Find loop by filtering edges going back to product
+    FILTER e._to == start
+
+    // Return product codes generating the loop
+    LET loop = (
+        FOR item in p.vertices
+        FILTER IS_SAME_COLLECTION(Product, item)
+        RETURN item.code
+    )
+    RETURN loop
+  """
+
 
 def get_bom_from_db(db, product_key):
   db_result = db.aql.execute(
@@ -43,4 +58,14 @@ def define_bom_line_for_db(bom_line_in):
   )
 
   return jsonable_encoder(bom_line_out, by_alias=True, exclude_none=True)
+
+
+def find_bom_loops(db, product_key):
+  """Make sure the product BoM has no loops in it"""
+  print('Checking bom loops')
+  bind_vars = dict(product_key=product_key)
+  cursor = db.aql.execute(Queries.CHECK_BOM_LOOP, bind_vars=bind_vars)
+  loops = [' -> '.join(l) for l in cursor]
+  print('Result: ', loops)
+  return loops
 

webapps/main/src/views/ProductBom.vue
@@ -422,7 +422,16 @@ export default {
         }, 1500)  
       })
       .catch( err => {
-        window.alert(err)
+        if (err.response.status == 403) {
+          const api_resp = err.response.data.detail
+          const error_message = api_resp.message
+                                + '\n\nLoops:\n'
+                                + api_resp.data.join('\n')
+          window.alert(error_message)
+        }
+        else {
+          window.alert(err)
+        }
         this.saving = false
       })
     },

commit 387b49894b61f0e2d81e98c4b1405a19daeb5d22

Close #170 - Fix Phase creation bug
Bug introduced with model update in b7acc367edc6fdfb7ad980bca7ba967e57d003b8

backend/api/endpoints/process.py
@@ -158,7 +158,7 @@ async def get_production_process(product_key):
   response_model = List[PhaseUpdate],
   response_model_exclude = {'step_sequence'}
 )
-async def update_process(process: List[PhaseUpdate], product_key):
+async def update_process(product_key, process: List[PhaseUpdate]):
  
   
   tx_db = db.begin_transaction(write=['Product', 'Phase', 'Step', 'requires'])
@@ -256,7 +256,6 @@ async def update_process(process: List[PhaseUpdate], product_key):
       error_str=error_str
     )
     tx_db.abort_transaction()
-    print(error_str)
     raise HTTPException(
       status_code = 500,
       detail = error_str

backend/api/endpoints/product.py
@@ -63,7 +63,7 @@ async def create_product(
 
   # Map form data
   try:
-    new_product = ProductData(code=code, description=description)
+    new_product = ProductDetails(code=code, description=description)
     prepped_data = jsonable_encoder(new_product, by_alias=True, exclude_none=True )
 
   except Exception as e:

webapps/main/src/views/ProductionOverview.vue
@@ -229,12 +229,12 @@ export default {
       bool_filters: {
         started: { value: true },
         queued: { value: true },
-        on_time: { value: true },
-        late: { value: true },
+        // on_time: { value: true },
+        // late: { value: true },
         active: { value: true },
         idle: { value: true },
-        critical: { value: true },
-        not_critical: { value: true }
+        //critical: { value: true },
+        //not_critical: { value: true }
         // with_open_issues_only: { label: 'Solo con segnalazioni aperte', value: true },
       },
       search_string: undefined,

webapps/main/src/views/ProductionProcess.vue
@@ -346,6 +346,7 @@ export default {
       new_process.push({ 
         alias: new_operation.name, 
         operation_key: new_operation._key, 
+        product_key: this.product_key,
         params: new_operation.default_phase_parameters,
         steps: []
       })