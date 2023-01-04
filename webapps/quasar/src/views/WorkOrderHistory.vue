05507108c536f17b7daaed726f99d7ed3e685b60
Luca Sorgiacomo <luca@progresslab.it>
2023-01-04 23:11:38 +0100

    Wire in new WorkOrderHistory component

---
+++ webapps/quasar/src/views/WorkOrderHistory.vue
@@ -0,0 +1,19 @@
+<template>
+  TEST
+</template>
+
+<script>
+export default {
+
+  name: 'WorkOrderHistory',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
