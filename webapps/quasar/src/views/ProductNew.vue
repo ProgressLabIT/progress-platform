adf292fb19d6cc91554bf11907338f574ee187df
Luca Sorgiacomo <luca@progresslab.it>
2023-01-20 22:10:57 +0100

    Draft ProductNew migration

--- webapps/quasar/src/views/ProductNew.vue
+++ webapps/quasar/src/views/ProductNew.vue
@@ -1,63 +1,53 @@
 <template>
-  <v-dialog 
-    value="true" 
-    :overlay-color="$theme.background"
-    overlay-opacity="1"
-    max-width="500px"
-    persistent no-click-animation>
-    <v-card>
-      <v-card-title>
-        <h3 class="display">{{ $tc('product.new_modal_title') }}</h3>
-      </v-card-title>
-      <v-card-text>
-        <v-form>  
-          <v-text-field
-            clearable
-            counter="20"
-            :label="$tc('code') | capitalize"
-            v-model="new_product_code"
-            class="input-uppercase"
-            />
-          <v-textarea
-            clearable
-            auto-grow
-            counter="500"
-            :label="$tc('description') | capitalize"
-            v-model="new_product_desc"
-            />
-          <v-file-input 
-            clearable
-            label="Immagine" 
-            accept="image/*"
-            prepend-icon=""
-            append-icon="image"
-            show-size
-            v-model="new_product_pic"
-            />
-        <v-row class="mt-6">
-          <v-col>
-            <v-btn block depressed color="primary" @click="postNewProduct">
-              {{ $tc('save') }}
-            </v-btn>
-          </v-col>
-          <v-col>    
-            <v-btn block depressed :color="$theme.grey" @click="$router.back()">
-              {{ $tc('cancel') }}
-            </v-btn>
-          </v-col>    
-        </v-row>  
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+  <BaseModalForm
+    id="new-product-form"
+    @submit="postNewProduct"
+    max_width="700px"
+    @cancel="$router.back()">
+
+    <template #title>
+      {{ $t('product.new_modal_title') }}
+    </template>
+
+    <template #form>
+      <q-input
+        :label="$capitalize($t('code'))"
+        v-model="new_product_code"
+        class="input-uppercase"
+        clearable>
+      </q-input>
+      <q-input
+        type="textarea"
+        clearable
+        :label="$capitalize($t('description'))"
+        v-model="new_product_desc">
+      </q-input>
+      <q-file
+        :label="$t('image')"
+        accept="image/*"
+        clearable
+        counter>
+        <template #append>
+          <q-icon name="mdi-image" />
+        </template>
+      </q-file>
+    </template>
+
+  </BaseModalForm>
 </template>
 
 <script>
-import { api } from '@/lib/apiCall.js'
+import { api } from '@/boot/axios.js'
+
+import BaseModalForm from '@/components/BaseDialog.vue'
 
 export default {
 
   name: 'NewProduct',
+
+  components: {
+    BaseModalForm
+  },
 
   data() {
     return {
@@ -103,9 +93,11 @@ export default {
           window.alert("Couldn't save product, try again.", error)
           this.$router.back()
         })
-        
-
     }
+  },
+
+  created() {
+    console.log('Creato')
   }
 };
 </script>
