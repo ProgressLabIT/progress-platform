b4ef5b115ddc58b0662130749ca84fc8d83fc65a
Luca Sorgiacomo <luca@progresslab.it>
2023-01-26 17:48:59 +0100

    Migrate PhaseParameters

--- webapps/quasar/src/components/PhaseParameters.vue
+++ webapps/quasar/src/components/PhaseParameters.vue
@@ -1,5 +1,64 @@
 <template>
-  <div>TEST</div>
+  <q-list>
+    <template
+      v-for="(p_value, p_key) in phase_params"
+      :key="p_key">
+
+      <q-expansion-item
+        :expand-icon="paramType(p_key) == 'int' || !edit_mode ? 'none' : ''"
+        :model-value="expansion_map == p_key"
+        @update:model-value="(value) => updateExpansionMap(p_key, value)">
+
+        <!-- SELECTED OPTION -->
+        <template #header>
+          <div class="full-width q-pa-lg">
+            <div class="text-h5 uppercase q-mb-sm low-text">
+              {{ paramHumanName(p_key, p_value) }}
+            </div>
+            <div
+              v-if="!edit_mode || paramType(p_key) != 'int'"
+              class="text-h3 highlight">
+              {{ paramHumanValue(p_key, p_value) }}
+            </div>
+            <q-input
+              v-else
+              type="number" min="0"
+              :readonly="!edit_mode"
+              :model-value="paramHumanValue(p_key)"
+              @update:model-value="(value) => updateParam(p_key, value)">
+            </q-input>
+            <div class="q-mt-sm">
+              {{ paramValueDesc(p_key, p_value) }}
+            </div>
+          </div>
+        </template>
+
+        <!-- OTHER OPTIONS -->
+        <template v-if="paramType(p_key) != 'int' && edit_mode">
+          <q-list>
+            <q-item
+              v-for="(value, index) in paramOtherValues(p_key, p_value)"
+              :key="index"
+              clickable
+              v-ripple
+              @click="updateParam(p_key, value)">
+              <q-item-label class="q-pa-lg">
+                <div class="text-h5 highlight q-mb-sm">
+                  {{ paramHumanValue(p_key, value) }}
+                </div>
+                <div>
+                  {{ paramValueDesc(p_key, value) }}
+                </div>
+              </q-item-label>
+            </q-item>
+          </q-list>
+        </template>
+      </q-expansion-item>
+
+      <q-separator />
+
+    </template>
+  </q-list>
 </template>
 
 <script>
@@ -42,6 +101,15 @@ export default {
 
     paramType(param_key) {
       return params_map[param_key].type
+    },
+
+    updateExpansionMap(param_key, expanded) {
+      if (expanded) {
+        this.expansion_map = param_key
+      }
+      else {
+        this.expansion_map = null
+      }
     },
 
     paramHumanValue(param_key, value_key) {
