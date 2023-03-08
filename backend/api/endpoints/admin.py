b6fc25470188bf12a13738941a64b2352b51ae0a
Luca Sorgiacomo <luca@progresslab.it>
2023-03-08 17:55:56 +0100

    Implement force delete work order api

--- backend/api/endpoints/admin.py
+++ backend/api/endpoints/admin.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter
+import traceback
+
+from fastapi import APIRouter, HTTPException
 
 from utils.db import db
+from models.production import WorkStatus
 
 router = APIRouter()
 
-@router.delete('/reset/prod')
-async def reset_production_and_traceability_data():
-  collections = [
+traceability_collections = [
     'Batch',
     'Event',
     'Job',
@@ -17,7 +18,10 @@ async def reset_production_and_traceability_data():
     'WorkSession',
   ]
 
-  tx = db.begin_transaction(write=collections)
+@router.delete('/reset/prod')
+async def reset_production_and_traceability_data():
+
+  tx = db.begin_transaction(write=traceability_collections)
 
   for c in collections:
     tx.collection(c).truncate()
@@ -31,3 +35,72 @@ async def reset_production_and_traceability_data():
   ))
 
   tx.commit_transaction()
+
+  return 'Reset of Production and Traceability data successful'
+
+
+async def get_work_order_jobs(work_order_key):
+  cursor = db.collection('Job').find(dict(wo_key=work_order_key))
+  return [j['_key'] for j in cursor]
+
+
+
+@router.delete('/force-delete-work-order/{work_order_key}')
+async def force_delete_work_order_data(work_order_key: str):
+
+  # Check if the work order actually exists
+  if not db.collection('WorkOrder').has(work_order_key):
+    raise HTTPException(status_code=404, detail="Work order key could not be found in the database")
+
+  # 0. Setup transaction
+  tx = db.begin_transaction(write=traceability_collections)
+
+  try:
+    match = dict(work_order_key=work_order_key)
+
+    # 1. Delete Events, Batches, wip, WorkSessions and WorkOrder based on work_order_key
+    tx.collection('Event').delete_match(match)
+    tx.collection('Batch').delete_match(match)
+    tx.collection('wip').delete_match(dict(wo_key=work_order_key))
+    tx.collection('WorkSession').delete_match(match)
+    tx.collection('WorkOrder').delete(work_order_key)
+
+    # 2. Delete jobs and get job list
+    job_delete_query = """
+      FOR j IN Job
+      FILTER j.wo_key == @work_order_key
+      REMOVE j IN Job
+      LET removed = OLD
+      RETURN removed._key
+    """
+    cursor = tx.aql.execute(job_delete_query, bind_vars=match)
+    jobs_to_delete = [j for j in cursor]
+
+    # 3. Delete StepExecutionData based on deleted job_key
+    step_data_delete_query = """
+      FOR s IN StepExecutionData
+      FILTER POSITION(@jobs_to_delete, s.job_key)
+      REMOVE s IN StepExecutionData
+    """
+    tx.aql.execute(step_data_delete_query, bind_vars=dict(jobs_to_delete=jobs_to_delete))
+
+    # 3. Delete Work Order and Jobs from Queue
+    queue_update_query = """
+      FOR q IN Queue
+      LET jobs = MINUS(q.jobs, @jobs_to_delete)
+      LET work_orders = REMOVE_VALUE(q.work_orders, @work_order_key)
+      UPDATE q WITH { jobs, work_orders } in Queue
+    """
+    bind_vars = dict(work_order_key=work_order_key, jobs_to_delete=jobs_to_delete)
+    tx.aql.execute(queue_update_query, bind_vars=bind_vars)
+
+    # 9. Commit and return
+    tx.commit_transaction()
+    return f"All data related to WorkOrder {work_order_key} has been deleted."
+
+  except Exception:
+    raise HTTPException(status_code=500, detail=traceback.format_exc())
+
+
+
+
