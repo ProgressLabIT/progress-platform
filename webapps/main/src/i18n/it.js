d69a40909752e2e8e18fd546c9b22893cbd4457c
Luca Sorgiacomo <luca@progresslab.it>
2023-03-08 12:46:27 +0100

    Capitalize job_closed message.

--- webapps/main/src/i18n/it.js
+++ webapps/main/src/i18n/it.js
@@ -95,7 +95,7 @@ export default {
       "batch_confirm": "Hai completato l'ultimo passo della procedura. Confermi il completamento dei pezzi in lavorazione?",
       "confirm_exit": "Vuoi davvero lasciare questa pagina? Il lavoro verrà messo in pausa.",
       "input_not_available": "Non c'è materiale disponibile per procedere, la sessione di lavoro verrà conclusa. Se ritieni che ciò non sia accurato contatta il tuo responsabile.",
-      "job_closed": "attenzione lavoro chiuso. reindirizzamento in corso",
+      "job_closed": "Attenzione lavoro chiuso. Reindirizzamento in corso...",
       "job_complete_confirm": "Hai completato tutti i pezzi previsti dal lavoro. Ne confermi la chiusura?",
       "rebalance_qt_mismatch": "le quantità non combaciano",
       "rebalance_unassigned_jobs": "assegna un operatore ai nuovi lavori"
