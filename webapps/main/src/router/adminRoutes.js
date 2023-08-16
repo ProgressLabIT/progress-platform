d6302d82242fdeb2678542662f8603f313b8e754
Luca Sorgiacomo <luca@progresslab.it>
2023-08-16 15:04:04 +0200

    Pass route props in admin sections

--- webapps/main/src/router/adminRoutes.js
+++ webapps/main/src/router/adminRoutes.js
@@ -31,7 +31,8 @@ const adminRoutes = [
           {
             path: ':user_key',
             name: 'userInfo',
-            component: () => import("@/components/UserInfoScreen.vue")
+            component: () => import("@/components/UserInfoScreen.vue"),
+            props: true
           },
           {
             path: 'new',
@@ -58,7 +59,8 @@ const adminRoutes = [
           {
             path: ':operation_key',
             name: 'operationDetail',
-            component: () => import("@/views/OperationDetail.vue")
+            component: () => import("@/views/OperationDetail.vue"),
+            props: true
           },
           {
             path: 'new',
