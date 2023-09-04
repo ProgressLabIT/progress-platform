30dcfd9c32cbd99e8655938846314e92e48d273b
Luca Sorgiacomo <luca@progresslab.it>
2023-09-04 12:11:06 +0200

    Add app section with iframe pointing to sample report
    - Open up port

---
+++ webapps/main/src/views/ReportRoot.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="absolute-full scroll">
+    <iframe id="iframe" :src="src" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReportRoot',
+
+  computed: {
+    src() {
+      // return window.location.hostname
+      return 'http://91.229.245.119:30002'
+    }
+  },
+
+  created() {
+    console.log(this.src)
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+#iframe
+  height: 100%
+  width: 100%
+</style>
