bd2569dacbb8ff829031f8d4bfd337a3775c8910
Luca Sorgiacomo <luca@progresslab.it>
2023-09-09 14:15:45 +0200

    Enforce permissions for reports area

--- webapps/main/src/router/reportRoutes.js
+++ webapps/main/src/router/reportRoutes.js
@@ -2,6 +2,7 @@ const reportRoutes = [
   {
     path: 'report',
     name: 'reportRoot',
+    meta: { scope: 'reporting', screen_title: 'Rapporti' },
     component: () => import("@/views/ReportRoot.vue")
   }
 ]
