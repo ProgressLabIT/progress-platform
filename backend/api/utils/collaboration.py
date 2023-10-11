7b7b04c0b6d13ae4ae7633909c2e9fefeceedb32
Luca Sorgiacomo <luca@progresslab.it>
2023-10-11 13:04:57 +0200

    feat: accept any falsy value for false boolean advanced filters
    When boolean forms fields are initialized they have a value of null.
    Forcing false as initial value was more complicated and required
    existing data updates, so instead when filter is false now it will
    match both false and null.

--- backend/api/utils/collaboration.py
+++ backend/api/utils/collaboration.py
@@ -42,9 +42,15 @@ class Queries:
       && (@issue_non_critical != null ? i.critical == !@issue_non_critical : true)
       && (@advanced_filters
         ? LENGTH((
+            // This subquery returns match true/false for each filter
             FOR advanced_filter IN NOT_NULL(@advanced_filters.filters, [])
             RETURN i.data
-              ? i.data[* FILTER CURRENT._key == advanced_filter._key AND (CURRENT.type == "choice" ? CURRENT.value.value == advanced_filter.value : CURRENT.value == advanced_filter.value)]
+              ? i.data[* FILTER CURRENT._key == advanced_filter._key
+                && advanced_filter.value == (
+                  CURRENT.type == "choice" ? CURRENT.value.value
+                  : CURRENT.type == "boolean" ? !!CURRENT.value
+                  : CURRENT.value
+                )]
               : []
           )[**]) >= (@advanced_filters.operator == "OR" ? 1 : LENGTH(@advanced_filters.filters))
         : true
