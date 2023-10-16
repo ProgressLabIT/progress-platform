bd35ce61f3ba83b3cd9074ba1415cb6e5fe1032c
Luca Sorgiacomo <luca@progresslab.it>
2023-10-16 10:16:32 +0200

    feat: add parameter to phase to allow unsupervised work

--- backend/api/models/process.py
+++ backend/api/models/process.py
@@ -26,6 +26,7 @@ class PhaseParameters(FlexModel):
   max_offline: int = 60 # seconds
   auto_new_batch: bool = True
   std_processing_time: int = 60 # seconds
+  allow_unsupervised_work: bool = False
   # release_style: ReleaseStyle = ReleaseStyle.JOB
   # release_batch_qt: int = 1
   # wip_flow: WIPFlow = WIPFlow.BUFFER
