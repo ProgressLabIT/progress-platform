bb42a9ac4d591c6a06c78cb8039f8860f71894b1
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2023-11-22 15:24:28 +0300

    fix: correctly handle media of default steps in operation and production

--- webapps/main/src/views/OperationNew.vue
+++ webapps/main/src/views/OperationNew.vue
@@ -2,9 +2,9 @@
   <BaseModalForm
     :show="true"
     id="new-operation-form"
-    @submit="submit"
     max_width="700px"
     @cancel="$router.back()"
+    @submit="submit"
   >
      <template #title>
       {{ $t('operation.new') }}
@@ -48,20 +48,12 @@
 import BaseModalForm from '@/components/BaseModalForm.vue'
 
 export default {
-
   name: 'OperationNew',
 
   components: { BaseModalForm },
 
   data () {
     return {
-      stage: 'form',
-      valid: true,
-      fields: [
-        { required: true, model: 'name', component: 'v-text-field' },
-        { required: false, model: 'code', component: 'v-text-field' },
-        { required: true, model: 'description', component: 'v-textarea' },
-      ],
       new_operation_data: {
         name: undefined,
         code: undefined,
@@ -71,33 +63,30 @@ export default {
   },
 
   methods: {
-    submit() {
+    async submit() {
       if (!this.new_operation_data.name) {
         window.alert(this.$capitalize(this.$t('operations.alerts.op_name_missing')))
         return
       }
 
-      this.$store.dispatch('createOperation', this.new_operation_data)
-        .then((new_operation_key) => {
-          this.$router.push({
-            name: 'operationDetail',
-            params: {
-              operation_key: new_operation_key
-            }
-          })
-        })
-        .catch(error => {
-          if (error.response.status === 409) {
-            window.alert(this.$capitalize(this.$t('operations.alerts.op_name_used')))
-          }
-          else {
-            window.alert(error)
+      try {
+        const new_operation_key = await this.$store.dispatch('createOperation', this.new_operation_data)
+
+        await this.$router.push({
+          name: 'operationDetail',
+          params: {
+            operation_key: new_operation_key
           }
         })
+      } catch (error) {
+        if (error.response.status === 409) {
+          window.alert(this.$capitalize(this.$t('operations.alerts.op_name_used')))
+        }
+        else {
+          window.alert(error)
+        }
+      }
     }
   }
 }
 </script>
-
-<style lang="css" scoped>
-</style>
