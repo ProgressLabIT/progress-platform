1b985748cb8adca8bb4782eceaeb0d89d5af1fc3
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2023-12-06 15:29:54 +0300

    fix: get field info into IssueDetail

--- webapps/main/src/components/IssueDetail.vue
+++ webapps/main/src/components/IssueDetail.vue
@@ -21,7 +21,6 @@
 
         <div class="row q-px-lg q-pt-md q-mb-md">
           <template v-if="issue.data.length > 0">
-            <!-- FIXME: issue.data only contains the value now, not the field info -->
             <div v-for="field in issue.data" :key="field._key" class="col-auto q-pr-md">
               <FormField
                 :field="field"
@@ -209,9 +208,22 @@ export default {
       return this.enrichIssue(issue_data)
     },
 
+    issue_type() {
+      return this.$store.getters.getIssueType(this.issue.issue_type_key)
+    },
+
+    form_fields() {
+      const form_template = this.issue_type?.form_template ?? []
+      return form_template.map(field => ({
+        ...field,
+        value: this.issue.data
+          .find(({ form_field_key }) => form_field_key === field._key)?.value
+      }))
+    },
+
     user_can_delete() {
       return this.$store.getters.hasPermission('production')
-    },
+    }
   },
 
   methods: {
