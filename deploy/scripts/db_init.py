ProgressLabIT/progress-platform

commit 442d72c0bedffe91acee1991b8d63eca2e6fe144

chore: add media collections to setup procedure
also correct alphabetical order of collections

deploy/scripts/db_init.py
@@ -85,9 +85,11 @@ collections = [
   'Department',
   'Event',
   'Issue',
-  'IssueType',
   'issue_rel',
+  'IssueType',
   'Job',
+  'Media',
+  'media_connection',
   'message',
   'Media',
   'media_connection',

commit d9829a199e7374b32f3e6d9fdbe9e308c4e695db

fix: duplicated collections in setup script
Regression introduced by 442d72c0bedffe91acee1991b8d63eca2e6fe144

deploy/scripts/db_init.py
@@ -91,8 +91,6 @@ collections = [
   'Media',
   'media_connection',
   'message',
-  'Media',
-  'media_connection',
   'Operation',
   'Phase',
   'Product',