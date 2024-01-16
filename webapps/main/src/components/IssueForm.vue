0dc508534946de10e12447bb5e68ba4bf1cfbd1b
Luca Sorgiacomo <luca@progresslab.it>
2024-01-16 09:58:25 +0100

    fix: issue data display in IssueForm
    see also c5e7599a6e56b0dd384310d31604a25eabd146b4

--- webapps/main/src/components/IssueForm.vue
+++ webapps/main/src/components/IssueForm.vue
@@ -366,7 +366,7 @@ export default {
       this.form_fields = form_template.map((field) => ({
         ...field,
         value: this.issue.data.find(
-          ({ form_field_key }) => form_field_key === field._key,
+          ({ _key }) => _key === field._key,
         )?.value,
       }));
     },
