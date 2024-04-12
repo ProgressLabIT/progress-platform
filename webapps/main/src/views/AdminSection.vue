411eefccf70410a1eb0301b37bb1da5d4c8ff8a0
Simone Belleli <bellelisi@gmail.com>
2024-04-12 15:06:19 +0000

    B/460

--- webapps/main/src/views/AdminSection.vue
+++ webapps/main/src/views/AdminSection.vue
@@ -21,9 +21,7 @@
       </q-tabs>
       <q-card class="surface1 col" square>
         <router-view v-slot="{ Component }" class="">
-          <keep-alive>
-            <component :is="Component" class="full-height" />
-          </keep-alive>
+          <component :is="Component" class="full-height" />
         </router-view>
       </q-card>
     </q-page>
