48a04fee66a03cf7cd1639229dbe4c43f1d58fbc
simone belleli <bellelisi@gmail.com>
2024-05-04 08:29:59 +0200

    464 get steps from product api

--- backend/api/endpoints/serial.py
+++ backend/api/endpoints/serial.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, HTTPException, Query
 from utils.api import APIResponse
 from models.form import CustomField, CustomListValue, FieldType
 from utils.db import db, model_to_db_dict
+from utils.serial import Queries
 
 router = APIRouter()
 
@@ -19,3 +20,11 @@ def fetch_field(name: str | None = None, key: str | None = None):
   cursor = db.collection('CustomField').find(match)
   result = [CustomField(**f) for f in cursor]
   return sorted(result, key=lambda x: x.name.lower())
+
+
+@router.get('/product-steps/{product_key}')
+def get_product_steps(product_key: str):
+  bind_vars = dict(
+    product_key = product_key
+  )
+  return [e for e in db.aql.execute(Queries.GET_PRODUCT_STEPS, bind_vars=bind_vars)]
