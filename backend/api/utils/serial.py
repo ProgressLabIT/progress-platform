0ba6e65d27c476f154be3c6716ea941c7b9fe10a
simone belleli <bellelisi@gmail.com>
2024-05-04 08:29:59 +0200

    464 get steps from product api

---
+++ backend/api/utils/serial.py
@@ -0,0 +1,14 @@
+
+class Queries:
+
+  GET_PRODUCT_STEPS = """
+    FOR phase IN Phase
+      FILTER phase.product_key == @product_key
+      RETURN {
+        alias: phase.alias,
+        steps: (
+          FOR step IN Step
+            FILTER step._key in phase.step_sequence
+            return step
+        )}
+  """
