6b4ccfed9cf147288ac6a4734bb0ed85a9e730b5
simone belleli <bellelisi@gmail.com>
2024-05-08 21:35:57 +0200

    464 serial custom field management

--- backend/commons/models/serial.py
+++ backend/commons/models/serial.py
@@ -2,17 +2,17 @@ from datetime import datetime
 from enum import Enum
 from pydantic import BaseModel
 from commons.models.base_models import FlexModel, ArangoEdge, ArangoDocument
-from commons.models.form import FormFieldValue
+from commons.models.form import SerialFormFieldValue
 
-class StepData(BaseModel):
-  step_key: str | None = None
-  title: str | None = None
-  data: list[FormFieldValue] | None = None
+#class StepData(BaseModel):
+#  step_key: str | None = None
+#  title: str | None = None
+#  data: list[FormFieldValue] | None = None#
 
-class PhaseData(BaseModel):
-  phase_hey: str | None = None
-  alias: str | None = None
-  steps: list[StepData] | None = None
+#class PhaseData(BaseModel):
+#  phase_key: str | None = None
+#  alias: str | None = None
+#  steps: list[StepData] | None = None
 
 class Serial(ArangoDocument):
    serial: str | None = None
@@ -21,10 +21,7 @@ class Serial(ArangoDocument):
    #wo_key: str
    created: datetime | None = None
    released: datetime | None = None
-   #locations: list[str]
-   #status: str
-   data: list[FormFieldValue] | None = None
-   phases: list[PhaseData] | None = None
+   data: list[SerialFormFieldValue] | None = None
 
 class SerialLinkType(str, Enum):
   PRODUCT = 'product'
