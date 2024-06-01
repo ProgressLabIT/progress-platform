1216a3a95c1eb4dfa08aefec3ab9833351e24f2c
simone belleli <bellelisi@gmail.com>
2024-06-01 18:58:17 +0200

    482 mandatory fields

--- webapps/main/src/i18n/en.js
+++ webapps/main/src/i18n/en.js
@@ -825,6 +825,7 @@ export default {
   workload_hours: 'Workload in hours',
   write: 'Write',
   yes: 'Yes',
+  mandatory: 'Mandatory',
   traceability: {
     enabled: 'Enable traceability',
     include_deleted: 'Include deleted',
