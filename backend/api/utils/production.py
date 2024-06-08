d5754303cdd533a5aae21fc8e1126ad528c2da8c
simone belleli <bellelisi@gmail.com>
2024-06-08 21:06:22 +0200

    482 REORDER_JOB_QUEUES improved

--- backend/api/utils/production.py
+++ backend/api/utils/production.py
@@ -88,6 +88,12 @@ class Queries:
         && (@target_key ? (IS_ARRAY(@target_key) ? q.subqueue_target_key IN @target_key : q.subqueue_target_key == @target_key) : true)
         && LENGTH(q.jobs)
 
+      LET queue_jobs = (
+        FOR j IN jobs
+        FILTER j.assigned_to == q.subqueue_target_key
+        RETURN j
+      )
+
       LET new_queue = REMOVE_VALUE(
         FLATTEN(
           FOR wo IN wo_queue
