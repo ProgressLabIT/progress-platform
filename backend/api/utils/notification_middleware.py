23f72ba9411b1239a89b65d72e123fbf3cffbafd
simone belleli <s.belleli@msitaly.com>
2024-06-10 17:00:40 +0200

    482 notification middleware

---
+++ backend/api/utils/notification_middleware.py
@@ -0,0 +1,24 @@
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+
+class NotificationMiddleware(BaseHTTPMiddleware):
+    def __init__(
+            self,
+            app
+    ):
+        super().__init__(app)
+
+    async def dispatch(self, request: Request, call_next):
+        # do something with the request object, for example
+        content_type = request.headers.get('Content-Type')
+        print(content_type)
+
+        # process the request and get the response
+        response = await call_next(request)
+        self.notify_change(request=request)
+
+        return response
+
+    def notify_change(self, request: Request):
+        if (request['method']=='GET'):
+            return
