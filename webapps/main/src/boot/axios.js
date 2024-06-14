4c9168ba04a75fd7eb05b8609e32c395ef62b7a3
simone belleli <bellelisi@gmail.com>
2024-06-14 19:52:50 +0200

    482 serial bugfix

--- webapps/main/src/boot/axios.js
+++ webapps/main/src/boot/axios.js
@@ -10,9 +10,9 @@ import { boot } from 'quasar/wrappers';
 
 const domain =
   window.location.hostname === 'localhost'
-    ? //? 'http://progress.localhost'
-      'http://localhost:8000'
-    : 'http://' + window.location.hostname;
+    ? 'http://progress.localhost'
+    : //  'http://localhost:8000'
+      'http://' + window.location.hostname;
 
 const api_base_path = '/api';
 
@@ -31,7 +31,7 @@ export default boot(({ app, store }) => {
       if (error) {
         if (
           error.response.status === 401 &&
-          error.config.url !== 'whoami/' &&
+          error.config.url !== 'whoami' &&
           !error.config.url.includes('session')
         ) {
           //originalRequest._retry = true;
