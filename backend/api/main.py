1b137db41234fab25f41c7a3a2d0bf36a3da8d1e
simone belleli <bellelisi@gmail.com>
2024-06-15 19:12:09 +0200

    482 CORS

--- backend/api/main.py
+++ backend/api/main.py
@@ -24,10 +24,13 @@ app = FastAPI(
 )
 # global_router = APIRouter()
 
+origins = [
+    "*",
+]
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:9000"],
+  allow_origins=origins,
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
