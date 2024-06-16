6ed1fa105442d23ebc59cee83905df3013efa6bb
simone belleli <bellelisi@gmail.com>
2024-06-16 16:48:09 +0200

    482 CORS + gzip

--- backend/api/middlewares/notification_middleware.py
+++ backend/api/middlewares/notification_middleware.py
@@ -10,10 +10,6 @@ class NotificationMiddleware(BaseHTTPMiddleware):
         super().__init__(app)
 
     async def dispatch(self, request: Request, call_next):
-        # do something with the request object, for example
-        content_type = request.headers.get('Content-Type')
-        print(content_type)
-
         # process the request and get the response
         response = await call_next(request)
         self.notify_change(request=request)
