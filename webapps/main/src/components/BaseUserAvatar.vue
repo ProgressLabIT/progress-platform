c879538bf066a6aa5806efa2146944e50a7d621a
Luca Sorgiacomo <luca@progresslab.it>
2024-06-24 18:06:45 +0200

    fix: improve code readability in BaseUserAvatar

--- webapps/main/src/components/BaseUserAvatar.vue
+++ webapps/main/src/components/BaseUserAvatar.vue
@@ -113,15 +113,7 @@ export default {
     },
 
     full_name() {
-      return (
-        this.$capitalizeAll(
-          this.user
-            ? this.user.name
-            : '' + ' ' + this.user
-              ? this.user.surname
-              : '',
-        ) || ''
-      );
+      return this.$capitalizeAll(this.user ? this.user.name + ' ' + this.user.surname : 'NA');
     },
   },
 };
