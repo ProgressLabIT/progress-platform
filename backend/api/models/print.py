979df6b1ad8f302f67bb21da551e870c79e9d6a0
simone belleli <s.belleli@msitaly.com>
2024-06-27 16:57:57 +0200

    449 Allow deletion of print templates

--- backend/api/models/print.py
+++ backend/api/models/print.py
@@ -88,6 +88,7 @@ class PrintTemplateRecord(ArangoDocument):
   description: str | None = None
   links: dict[str, PrintTemplateLink] = dict()
   template: PrintTemplate | None = None
+  entities: int | None = 0
 
 
 class TemplateAssignment(ArangoEdge):
