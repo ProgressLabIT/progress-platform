f25bb36725de4c6648e77c832f6c5e5272d5362a
Luca Sorgiacomo <luca@progresslab.it>
2024-07-22 12:14:19 +0200

    hotfix: optimize job reordering query
    Avoid reconstructing queue job data at each iteration

--- backend/api/utils/production.py
+++ backend/api/utils/production.py
@@ -88,11 +88,13 @@ class Queries:
         && (@target_key ? (IS_ARRAY(@target_key) ? q.subqueue_target_key IN @target_key : q.subqueue_target_key == @target_key) : true)
         && LENGTH(q.jobs)
 
+      LET queue_jobs = q.jobs[* RETURN DOCUMENT(Job, CURRENT)]
+
       LET new_queue = REMOVE_VALUE(
         FLATTEN(
           FOR wo IN wo_queue
             FOR phase IN wo.phase_sequence
-              FOR j IN q.jobs[* RETURN DOCUMENT(Job, CURRENT)]
+              FOR j IN queue_jobs
               FILTER
                 j.wo_key == wo._key
                 && j.phase_key == phase
