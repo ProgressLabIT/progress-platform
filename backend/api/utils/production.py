1b776696664ad41ebb9a11d00310cbd736114033
Luca Sorgiacomo <luca@progresslab.it>
2024-07-22 12:49:36 +0200

    hotfix: optimize job reordering query
    Loop over the entire collection exploiting the wo+phase index

--- backend/api/utils/production.py
+++ backend/api/utils/production.py
@@ -88,13 +88,11 @@ class Queries:
         && (@target_key ? (IS_ARRAY(@target_key) ? q.subqueue_target_key IN @target_key : q.subqueue_target_key == @target_key) : true)
         && LENGTH(q.jobs)
 
-      LET queue_jobs = q.jobs[* RETURN DOCUMENT(Job, CURRENT)]
-
       LET new_queue = REMOVE_VALUE(
         FLATTEN(
           FOR wo IN wo_queue
             FOR phase IN wo.phase_sequence
-              FOR j IN queue_jobs
+              FOR j IN Job
               FILTER
                 j.wo_key == wo._key
                 && j.phase_key == phase
