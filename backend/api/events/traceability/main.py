ac0c93d8ca0655542ce8eed1bcd2f15656d64460
Luca Sorgiacomo <luca@progresslab.it>
2024-08-14 18:19:28 +0200

    refactor: migrate work session utility methods

--- backend/api/events/traceability/main.py
+++ backend/api/events/traceability/main.py
@@ -120,48 +120,11 @@ class ProductionActivityEvent(BaseEvent):
   # HELPER METHODS (Updates to specific collections)
   ######################################################################
 
-  # ===================================================================
-  # WorkSession
-  # ===================================================================
-
-  def create_work_session(self):
-    # Close unallowed parallel work sessions
-    bind_vars = dict(
-      user_key = self.info.user_key,
-      timestamp = self.info.timestamp
-    )
-
-    closed_sessions_cursor = self.tx.aql.execute(
-      TraceabilityQueries.CLOSE_UNALLOWED_PARALLEL_WORK_SESSIONS,
-      bind_vars=bind_vars
-    )
-    closed_sessions_jobs = [ws['job_key'] for ws in closed_sessions_cursor]
-    if len(closed_sessions_jobs):
-      self.tx.aql.execute(
-        """
-        FOR j IN Job
-        FILTER j._key IN @jobs
-        UPDATE j WITH { 'active': false } IN Job
-        """,
-        bind_vars=dict(jobs=closed_sessions_jobs)
-      )
-
-    # Create new work session
-    new_work_session = self.tx.aql.execute(
-      TraceabilityQueries.CREATE_WORK_SESSION, bind_vars=dict(
-        job_key = self.job.key,
-        batch_key = self.batch.key, # in self info can be under new_batch_key or active_batch_key, taking it from self.batch makes it more consistent.
-        work_order_key = self.job.wo_key,
-        phase_key = self.job.phase_key,
-        product_key = self.job.product_key,
-        user_key = self.info.user_key,
-        user_session_key = self.info.user_session_key,
-        start = self.info.timestamp,
-      )
-    ).next()
-    self.work_session = WorkSession(**new_work_session)
-    self.info.work_session_key = self.work_session.key
-
+  from .worksession import (
+    create_work_session,
+    get_current_work_session,
+    close_work_session
+  )
 
   # ===================================================================
   # Serial
