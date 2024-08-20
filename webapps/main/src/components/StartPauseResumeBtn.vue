08fc1985c98ac59ca19b07231207954e66f0415a
Luca Sorgiacomo <luca@progresslab.it>
2024-08-20 15:32:33 +0200

    fix: select new batch serials only if no active batch is present
    also lint StartPauseResumeBtn file

--- webapps/main/src/components/StartPauseResumeBtn.vue
+++ webapps/main/src/components/StartPauseResumeBtn.vue
@@ -52,13 +52,18 @@ export default {
         return result;
       } else {
         // Check if progress has already been made or user has already started
-        if (this.j.traceability_level && !this.j.first_phase) {
+        if (
+          this.j.traceability_level &&
+          !this.j.first_phase &&
+          !this.j.active_batch_key
+        ) {
           result.text = this.$t('job.link_serials');
           result.action = this.selectSerialWipAndStartSession;
           return result;
         } else if (this.j.stage == 'started') {
           result.text = this.$t('job.resume').toUpperCase();
-          result.action = () => this.$store.dispatch('resumeJob', { batch_serials: null });
+          result.action = () =>
+            this.$store.dispatch('resumeJob', { batch_serials: null });
           return result;
         } else {
           result.text = this.$t('job.start').toUpperCase();
@@ -70,28 +75,33 @@ export default {
     },
 
     async selectSerialWipAndStartSession() {
-      Loading.show()
+      Loading.show();
       const params = {
         wo_key: this.j.wo_key,
-        phase_key: this.j.phase_key
-      }
-      const { data: available_serials } = await this.$api.get('wip-serial', { params })
-      Loading.hide()
+        phase_key: this.j.phase_key,
+      };
+      const { data: available_serials } = await this.$api.get('wip-serial', {
+        params,
+      });
+      Loading.hide();
       let selected_serials = await new Promise((resolve) => {
         Dialog.create({
           component: SerialBatchSelectionDialog,
           componentProps: {
-            available_serials: available_serials.map(s => ({ label: s.serial_code, value: s.serial_key })),
+            available_serials: available_serials.map((s) => ({
+              label: s.serial_code,
+              value: s.serial_key,
+            })),
             selected_serials: [],
-            max_quantity: this.j.qt_planned - this.j.qt_completed
+            max_quantity: this.j.qt_planned - this.j.qt_completed,
           },
         })
-        .onOk((selected_serials) => resolve(selected_serials))
-        .onCancel(() => resolve(false));
+          .onOk((selected_serials) => resolve(selected_serials))
+          .onCancel(() => resolve(false));
       });
       if (selected_serials) {
-        console.log(selected_serials)
-        const action = this.j.stage == 'created' ? 'startJob' : 'resumeJob'
+        console.log(selected_serials);
+        const action = this.j.stage == 'created' ? 'startJob' : 'resumeJob';
         this.$store.dispatch(action, {
           batch_serials: selected_serials,
         });
