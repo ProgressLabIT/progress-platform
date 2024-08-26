5f8d5de929ac12c4e201d9cd5470b1fed280e6d0
Luca Sorgiacomo <luca@progresslab.it>
2024-08-26 14:20:34 +0200

    chore: adjust axios config for debug

--- webapps/main/src/boot/axios.js
+++ webapps/main/src/boot/axios.js
@@ -10,7 +10,7 @@ import { boot } from 'quasar/wrappers';
 
 const domain =
   window.location.hostname === 'localhost'
-    ? 'http://progress.localhost'
+    ? 'http://0.0.0.0:8000'
     : 'http://' + window.location.hostname;
 
 const api_base_path = '/api';
