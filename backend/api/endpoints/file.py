4a714366032facae61a6ef0c6fc3f239ab402bfa
Luca Sorgiacomo <luca@progresslab.it>
2024-09-03 15:56:40 +0200

    fix: imports in endpoints/file
    product metadata feature was developed starting from DEV before the
    merge of multilevel traceability, where the file structure changed

--- backend/api/endpoints/file.py
+++ backend/api/endpoints/file.py
@@ -3,8 +3,8 @@ import traceback
 
 from fastapi import APIRouter, Body, Depends, Form, HTTPException, UploadFile
 
-from models.form import FileBucket, FileTargetData
-from utils.db import db
+from commons.models.form import FileBucket, FileTargetData
+from commons.utils.db import db
 from utils.file import FileHandler
 
 
