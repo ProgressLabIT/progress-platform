902345ca3e7f332b7956f1bc257e1cb9af843433
Luca Sorgiacomo <luca@progresslab.it>
2024-10-10 10:20:58 +0200

    feat: reuse client for pausing multiple jobs

--- backend/workflow/flows/flowcode/pause_offline_jobs.py
+++ backend/workflow/flows/flowcode/pause_offline_jobs.py
@@ -44,7 +44,7 @@ def check_offline_jobs() -> list:
   return result
 
 @task
-def pause_job(job_data):
+def pause_job(api, job_data):
   """
   Use the event API to pause jobs
   e.g.
@@ -56,24 +56,26 @@ def pause_job(job_data):
   api.event(event_data)
   """
   job_key = job_data['_key']
+
   print(f'Pausing job {job_key}...')
-  with setup_progress_client() as api:
-    event_data = dict(
-      event_type = 'JOB_PAUSED_OFFLINE',
-      job_key = job_key,
-      work_session_end = job_data['last_online'],
-      user_key = 'wf:pause_offline_jobs',
-      user_session_key = 'wf',
-      description = "Exceeded max offline time allowed"
-    )
-    try:
-      print('Calling API...', end="")
-      r = api.post('/event', json=event_data)
-      print('Response: ', r.json())
-      if r.status_code == 200:
-        print(f"Job {job_key} paused.")
-    except Exception as e:
-      print(f'Error while pausing job {job_key}: {e}')
+
+  event_data = dict(
+    event_type = 'JOB_PAUSED_OFFLINE',
+    job_key = job_key,
+    work_session_end = job_data['last_online'],
+    user_key = 'wf:pause_offline_jobs',
+    user_session_key = 'wf',
+    description = "Exceeded max offline time allowed"
+  )
+
+  try:
+    print('Calling API...', end="")
+    r = api.post('/event', json=event_data)
+    print('Response: ', r.json())
+    if r.status_code == 200:
+      print(f"Job {job_key} paused.")
+  except Exception as e:
+    print(f'Error while pausing job {job_key}: {e}')
 
 
 @flow(name="Pause offline jobs", log_prints=True)
@@ -82,8 +84,9 @@ def main():
   Pause jobs that have been offline for more seconds than indicated in the `max_offline` parameter of the job
   """
   jobs_to_pause = check_offline_jobs()
-  for job in jobs_to_pause:
-    pause_job(job)
+  with setup_progress_client() as api:
+    for job in jobs_to_pause:
+      pause_job(api, job)
 
 
 schedule = CronSchedule(cron="0 * * * *")
