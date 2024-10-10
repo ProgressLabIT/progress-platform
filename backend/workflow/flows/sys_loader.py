08868dffff2b18b5d09ef8dc82aa23880f95074e
Luca Sorgiacomo <luca@progresslab.it>
2024-10-10 10:06:55 +0200

    chore: update system workflow loader script to most recent specs
    the schedule parameter is deprecated in favor of "schedules" (plural)
    supporting a list of schedule.
    
    Also added basic logging.

--- backend/workflow/flows/sys_loader.py
+++ backend/workflow/flows/sys_loader.py
@@ -17,29 +17,31 @@ This script loads the flows contained in the each folder under the `flowcode` di
 """
 
 
-
-
-
 # run script to build and apply the flow
 for entry in flow_list:
   # Strip .py from entry name
   entry_name = entry.name[:-3]
+  print(f"Loading {entry_name}...", end="")
 
   # Load modules
   flow = importlib.util.spec_from_file_location(entry_name, entry.path).loader.load_module()
 
-  try:
-    schedule = flow.schedule
-  except AttributeError:
-    schedule = None
-
-  deployment = Deployment.build_from_flow(
+  deploy_options = dict(
     flow=flow.main,
     name="main",
     description=flow.main.__doc__.strip(),
     work_queue_name='system',
-    schedule=schedule,
     output=f'deployments/{entry_name}.yaml',
     skip_upload=True,
     apply=True
   )
+
+  try:
+    deploy_options.update(schedules=[flow.schedule])
+  except AttributeError:
+    # Do not add schedules if not present in the flow
+    pass
+
+  deployment = Deployment.build_from_flow(**deploy_options)
+
+  print("Done.")
