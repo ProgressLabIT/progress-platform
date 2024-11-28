2402d5b4b8a01eac2007b5a50abdf18cccb6c994
simone belleli <s.belleli@msitaly.com>
2024-11-28 11:43:02 +0100

    TEST label

--- webapps/warehouse/src/components/print/PrintLabelForm.vue
+++ webapps/warehouse/src/components/print/PrintLabelForm.vue
@@ -224,8 +224,10 @@ export default {
       let inputs = this.prepareInputs();
 
       generate({ template, inputs }).then(
-        (data) => {
-          printer.device.sendFile(new Blob(data));
+        //(data) => {
+          () => {
+          //printer.device.sendFile(new Blob(data));
+          printer.device.sendAsync("^XA^FO200,200^A0N36,36^FDTest Label^FS^XZ");
           this.loading_label = undefined;
           setTimeout(() => {
             this.loading_label = undefined;
