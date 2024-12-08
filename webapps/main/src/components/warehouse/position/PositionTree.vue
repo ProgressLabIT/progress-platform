360ebdb4e4a8d7b7c58d7bf9178f8d7cc26c2138
simone belleli <bellelisi@gmail.com>
2024-12-08 16:47:42 +0100

    position tree

--- webapps/main/src/components/warehouse/position/PositionTree.vue
+++ webapps/main/src/components/warehouse/position/PositionTree.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="!mini_state" class="q-pa-md q-gutter-sm">
     <q-tree
-      id="serial-tree"
-      ref="serialNodes"
+      id="position-tree"
+      ref="positionNodes"
       v-model:selected="selected"
       :nodes="nodes"
       node-key="key"
@@ -16,22 +16,14 @@
           @mouseenter="over_key = prop.node.key"
           @mouseleave="over_key = null"
         >
-          <div
-            :class="{
-              'text-disabled': prop.node.replaced && prop.node.key !== selected,
-              'text-weight-bold text-high': prop.node.key === selected,
-              'text-low': !prop.node.replaced && prop.node.key !== selected,
-            }"
-          >
-            {{ prop.node.product_code }}
-          </div>
+          # {{ prop.node.label }}
           <div class="absolute-right q-py-xs">
-            <q-badge v-if="prop.node.replaced" color="theme-grey" outline>
+            <q-badge v-if="prop.node.deleted" color="theme-grey" outline>
               <q-icon name="mdi-link-off" size="12px" />
             </q-badge>
           </div>
-          <q-btn
-            v-if="prop.node.parent_key && !prop.node.replaced && !edit_mode"
+          <!--- <q-btn
+            v-if="prop.node.parent_key && !prop.node.deleted && !edit_mode"
             v-show="over_key === prop.node.key"
             flat
             round
@@ -40,25 +32,7 @@
             @click.stop="editComponentLink(prop.node)"
           >
             <q-icon name="mdi-pencil" size="xs" />
-          </q-btn>
-          <q-tooltip
-            v-if="prop.node.product_description"
-            anchor="bottom middle"
-            self="top middle"
-          >
-            {{ prop.node.product_description }}
-          </q-tooltip>
-        </div>
-      </template>
-
-      <template #default-body="prop">
-        <div
-          :class="{
-            'text-weight-bold': prop.node.key === selected,
-            'text-disabled': prop.node.replaced,
-          }"
-        >
-          # {{ prop.node.label }}
+          </q-btn> -->
         </div>
       </template>
     </q-tree>
@@ -66,12 +40,8 @@
 </template>
 
 <script>
-import { Dialog } from 'quasar';
-import SerialComponentLinkEditDialog from '@/components/traceability/SerialComponentLinkEditDialog.vue';
-import { timestamp } from '@/lib/TimeHandling.js';
-
 export default {
-  name: 'SerialTree',
+  name: 'PositionTree',
 
   props: {
     mini_state: {
@@ -82,7 +52,7 @@ export default {
       type: Boolean,
       default: false,
     },
-    serial_key: {
+    position_key: {
       type: String,
       required: true,
     },
@@ -112,8 +82,8 @@ export default {
 
   watch: {
     selected: {
-      handler(serial_key) {
-        if (!serial_key) {
+      handler(position_key) {
+        if (!position_key) {
           this.selected = this.nodes[0].key;
         }
         this.$emit('select', this.selected);
@@ -122,10 +92,10 @@ export default {
   },
 
   created() {
-    this.getSerialHierarcy();
+    this.getPositionHierarcy();
     setTimeout(() => {
-      if (this.$refs.serialNodes) {
-        this.$refs.serialNodes.expandAll();
+      if (this.$refs.positionNodes) {
+        this.$refs.positionNodes.expandAll();
       }
     }, 500);
   },
@@ -138,10 +108,10 @@ export default {
       }, 1000);
     },
 
-    showSerialDetails(serialKey) {
+    showPositionDetails(positionKey) {
       const to_route = {
-        name: 'serialDetail',
-        params: { serialKey },
+        name: 'positionDetail',
+        params: { positionKey },
         query: {
           back_to: this.$route.name,
           ...this.$route.query,
@@ -161,40 +131,38 @@ export default {
     },
 
     convertNode(node, parent_key) {
-      let label = node?.serial_code || node.serial_key;
+      let label = node?.code || node.position_key;
       let children_data = [];
       let expandable = false;
       if (node?.children) {
-        children_data = this.getChildren(node.children, node.serial_key);
+        children_data = this.getChildren(node.children, node.position_key);
         expandable = true;
       }
       return {
-        key: node.serial_key,
+        key: node.position_key,
         parent_key: parent_key,
         label: label,
         //lazy: false,
         expandable: expandable,
         selectable: this.edit_mode ? false : true,
         children: children_data,
-        replaced: node.replaced,
+        deleted: node.deleted,
         product_key: node.product_key,
-        product_code: node.product_code,
-        product_description: node.product_description,
       };
     },
 
-    async getSerialHierarcy() {
+    async getPositionHierarcy() {
       this.loading = true;
 
-      const { data } = await this.$api.get('serial-hierarchy', {
+      const { data } = await this.$api.get('position-hierarchy', {
         params: {
-          serial_key: this.serial_key,
+          position_key: this.position_key,
         },
       });
 
-      this.selected = this.serial_key;
+      this.selected = this.position_key;
 
-      this.selected = this.serial_key;
+      this.selected = this.position_key;
 
       this.nodes_data = data;
 
@@ -204,95 +172,11 @@ export default {
 
       this.loading = false;
     },
-
-    editComponentLink(node) {
-      Promise.all([
-        this.$store.dispatch('appendSerial', {
-          serial_key: node.parent_key,
-        }),
-
-        this.$store.dispatch('appendSerial', {
-          serial_key: node.key,
-        }),
-      ]).then((values) => {
-        this.showEditComponentLink(node, values[0], values[1]);
-      });
-    },
-
-    showEditComponentLink(node, parent_serial, serial) {
-      let serialModel = {
-        _key: serial._key,
-        label: serial.code,
-        product_key: serial.product_key,
-        wo_key: parent_serial.wo_key,
-        value: serial._key,
-        reason: '',
-      };
-
-      let initial_values = [];
-      initial_values.push(serialModel);
-
-      Dialog.create({
-        component: SerialComponentLinkEditDialog,
-        componentProps: {
-          node: node,
-          serial: serialModel,
-          initial_values: initial_values,
-        },
-      }).onOk((new_values) => {
-        this.saveNewComponentLink(node, parent_serial, serial, new_values);
-      });
-    },
-
-    async saveNewComponentLink(node, parent_serial, serial, new_values) {
-      if (new_values) {
-        let link_data = [];
-        link_data.push({
-          wo_key: parent_serial.wo_key,
-          component_key: serial.product_key,
-          from_serial: node.parent_key,
-          to_serial: serial._key,
-          reason: new_values.reason,
-          replaced: true,
-          link_serial_directly: true,
-        });
-
-        link_data.push({
-          wo_key: parent_serial.wo_key,
-          component_key: serial.product_key,
-          batch_key: this.batch_key,
-          from_serial: node.parent_key,
-          to_serial: new_values._key,
-          reason: null,
-          replaced: false,
-          link_serial_directly: true,
-        });
-
-        const event = {
-          event_type: 'SERIAL_LINKED',
-          user_key: this.session_data.session_key,
-          timestamp: timestamp(),
-          wo_key: serial.wo_key,
-          serial_link_data: link_data,
-        };
-
-        await this.$api.post('event', event).then(() => {
-          this.nodes = [];
-          this.getSerialHierarcy();
-          setTimeout(() => {
-            if (this.$refs.serialNodes) {
-              this.$refs.serialNodes.expandAll();
-            }
-          }, 500);
-        });
-        //this.$emit('close');
-      }
-    },
   },
 };
 </script>
 
 <style lang="sass">
-#serial-tree .q-tree__node-body
+#position-tree .q-tree__node-body
   padding-top: 0px !important
 </style>
