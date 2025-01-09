693176f20dee7d033e8337d36719973c0f0fa10b
Luca Sorgiacomo <luca@progresslab.it>
2025-01-09 15:41:53 +0100

    feat: sort serial codes in transfer confirmation

--- webapps/warehouse/src/components/transfer/TransferManualConfirm.vue
+++ webapps/warehouse/src/components/transfer/TransferManualConfirm.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="row q-col-gutter-x-xs q-mt-sm">
       <!-- Show first 3 serial numbers as chips -->
-      <div v-for="item in transfer.contents" :key="item._key" class="col-auto">
+      <div v-for="item in transfer.contents.sort((a, b) => a.code.localeCompare(b.code))" :key="item._key" class="col-auto">
         <ContentChip :item="item" />
       </div>
       <!-- Show count of remaining serials if more than 3 are selected -->
