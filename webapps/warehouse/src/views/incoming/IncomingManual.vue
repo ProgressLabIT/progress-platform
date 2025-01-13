9a8f95588891c3ab626d3d729587c50c271abc65
Luca Sorgiacomo <luca@progresslab.it>
2025-01-13 22:55:04 +0100

    refactor: rename incoming components more explicitly
    to distinguish between similar components in shipment flow or others

--- webapps/warehouse/src/views/incoming/IncomingManual.vue
+++ webapps/warehouse/src/views/incoming/IncomingManual.vue
@@ -8,23 +8,29 @@
 </template>
 
 <script setup>
-import ConfirmPositionsPage from '@/components/incoming/position/ConfirmPositionsPage.vue';
-import PositionsPage from '@/components/incoming/position/PositionsPage.vue';
-import ProductsList from '@/components/incoming/products/ProductsList.vue';
-import QuantitySelectionPage from '@/components/incoming/quantity/QuantitySelectionPage.vue';
-import SerialSelectionPage from '@/components/incoming/quantity/SerialSelectionPage.vue';
+import IncomingConfirmPositionsPage from '@/components/incoming/position/IncomingConfirmPositionsPage.vue';
+import IncomingPositionsPage from '@/components/incoming/position/IncomingPositionsPage.vue';
+import IncomingProductsList from '@/components/incoming/products/IncomingProductsList.vue';
+import IncomingQuantitySelectionPage from '@/components/incoming/quantity/IncomingQuantitySelectionPage.vue';
+import IncomingSerialSelectionPage from '@/components/incoming/quantity/IncomingSerialSelectionPage.vue';
 import { useIncomingStore } from '@/stores/incoming';
 import { onBeforeRouteLeave } from 'vue-router';
 
 const incoming = useIncomingStore();
 
+// RECEIPT FLOW
+// 1. product
+// 2a. quantity
+// 2b. serials
+// 3. position (destination)
+// 4. confirm
 
 const stageComponentMap = {
-  product: ProductsList,
-  quantity: QuantitySelectionPage,
-  serials: SerialSelectionPage,
-  position: PositionsPage,
-  confirm: ConfirmPositionsPage,
+  product: IncomingProductsList,
+  quantity: IncomingQuantitySelectionPage,
+  serials: IncomingSerialSelectionPage,
+  position: IncomingPositionsPage, // destination position
+  confirm: IncomingConfirmPositionsPage,
 };
 
 onBeforeRouteLeave(() => {
