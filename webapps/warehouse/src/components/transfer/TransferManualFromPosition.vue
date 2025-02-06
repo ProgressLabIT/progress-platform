ProgressLabIT/progress-platform

commit 388cc8d9f18256ef17f6464a6b240095e1c70e83

feat: check inventory and adjust inventory quantity (wip)
for product with no serial only

webapps/warehouse/src/components/transfer/TransferManualFromPosition.vue
@@ -17,7 +17,7 @@
       <div class="col scroll">
         <div class="row full-width q-col-gutter-x-sm q-mt-md">
           <div v-for="pos in positionResults" :key="pos._key" class="col-auto">
-            <q-chip clickable outline class="text-body1" @click="selectPosition(pos)">
+            <q-chip flat outline class="text-body1" @click="selectPosition(pos)">
               {{ pos.code }}
             </q-chip>
           </div>
@@ -47,7 +47,7 @@
       :model-value="showContentTypeSelection !== null"
       @hide="showContentTypeSelection = null"
     >
-      <TransferManualPositionAction
+      <PositionAction
         :position="showContentTypeSelection"
         @select-container="selectContainer"
         @select-contents="selectContents(showContentTypeSelection)"
@@ -63,7 +63,7 @@ import { useI18n } from 'vue-i18n';
 import { api } from '@/boot/axios';
 import SearchOrScan from '@/components/SearchOrScan.vue';
 import SlideUpCard from '@/components/SlideUpCard.vue';
-import TransferManualPositionAction from '@/components/transfer/TransferManualPositionAction.vue';
+import PositionAction from '@/components/PositionAction.vue';
 import { useTransferStore } from '@/stores/transfer';
 import { useRouter } from 'vue-router';
 

webapps/warehouse/src/components/AppFooter.vue
@@ -56,8 +56,7 @@
               :icon="tab_routes[tab]"
               :label="$t(`views.${tab}`)"
               @click="forceHide"
-            >
-            </q-route-tab>
+            />
           </q-tabs>
         </q-card-section>
       </q-card>

webapps/warehouse/src/components/QuantitySelector.vue
@@ -139,7 +139,7 @@ const props = defineProps({
   },
   max: {
     type: Number,
-    default: 999999999999,
+    default: undefined,
   },
   softMax: {
     // Allow the quantity to go above the max with a warning
@@ -189,7 +189,7 @@ function setQuantity(value) {
   if (props.softMax) {
     quantity.value = Math.max(props.min, value);
   } else {
-    quantity.value = Math.max(props.min, Math.min(props.max, value));
+    quantity.value = Math.max(props.min, Math.min(props.max ?? 999999999999, value));
   }
 }
 

webapps/warehouse/src/components/transfer/TransferManualContents.vue
@@ -62,7 +62,7 @@
       @hide="cardItem = null"
     >
       <!-- Transfer container or use as start position  -->
-      <TransferManualPositionAction
+      <PositionAction
         v-if="cardItem.type === 'position'"
         :position="cardItem"
         @select-contents="changeStartPosition"
@@ -92,7 +92,7 @@ import { useTransferStore } from '@/stores/transfer';
 import QuantitySelector from '@/components/QuantitySelector.vue';
 import SearchOrScan from '@/components/SearchOrScan.vue';
 import SlideUpCard from '@/components/SlideUpCard.vue';
-import TransferManualPositionAction from '@/components/transfer/TransferManualPositionAction.vue';
+import PositionAction from '@/components/PositionAction.vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
@@ -215,7 +215,7 @@ function cancel() {
 
 </script>
 
-<style scoped lang="sass">
+<style lang="sass">
 .content-card
   border-radius: 5px
 </style>

webapps/warehouse/src/i18n/it.js
@@ -17,7 +17,8 @@ export default {
     transfer_lists: 'Liste',
     shipment_root: 'Spedizioni',
     shipment_manual: 'Nuovo',
-    shipment_lists: 'Liste'
+    shipment_lists: 'Liste',
+    inventory_root: 'Giacenze',
   },
   cancel: 'Annulla',
   close: 'Chiudi',

webapps/warehouse/src/router/inventoryRoutes.js
@@ -1,9 +1,27 @@
 const inventoryRoutes = [
   {
     path: 'inventory',
-    name: 'inventoryRoot',
+    name: 'InventoryRoot',
     component: () => import('@/views/inventory/InventoryRoot.vue'),
-    meta: { scope: 'warehouse', screen_title: 'Inventory' },
+    meta: { scope: 'warehouse', title: 'breadcrumb.inventory_root' },
+    redirect: { name: 'InventoryHome' },
+    children: [
+      {
+        path: '',
+        name: 'InventoryHome',
+        component: () => import('app/src/views/inventory/InventoryHome.vue'),
+      },
+      {
+        path: 'product',
+        name: 'InventoryProduct',
+        component: () => import('app/src/views/inventory/InventoryProduct.vue'),
+      },
+      {
+        path: 'position',
+        name: 'InventoryPosition',
+        component: () => import('app/src/views/inventory/InventoryPosition.vue'),
+      },
+    ],
   },
 ];
 

webapps/warehouse/src/stores/inventory.js
@@ -1,0 +1,19 @@
+import { defineStore } from 'pinia';
+import { api } from '@/boot/axios';
+
+export const useInventoryStore = defineStore('inventory', {
+  state: () => ({
+    position: null,
+    contents: [],
+  }),
+  actions: {
+    async loadInventory(params) {
+      const response = await api.get('/inventory', { params });
+      this.contents = response.data;
+    },
+    async loadPositionContents(position_key) {
+      const response = await api.get(`/position/${position_key}`);
+      this.contents = response.data;
+    }
+  }
+});

commit 843b01862783d109b4e60d36b76e633bed18da63

fix: regression in 388cc8d9f18

webapps/warehouse/src/components/transfer/TransferManualFromPosition.vue
@@ -17,7 +17,7 @@
       <div class="col scroll">
         <div class="row full-width q-col-gutter-x-sm q-mt-md">
           <div v-for="pos in positionResults" :key="pos._key" class="col-auto">
-            <q-chip flat outline class="text-body1" @click="selectPosition(pos)">
+            <q-chip clickable outline class="text-body1" @click="selectPosition(pos)">
               {{ pos.code }}
             </q-chip>
           </div>