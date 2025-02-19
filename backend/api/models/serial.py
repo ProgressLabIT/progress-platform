8a5e96211326d568e1f4059876f27bb373308589
Luca Sorgiacomo <luca@progresslab.it>
2025-02-19 16:47:48 +0100

    fix: remove available property in Serials. Not used and created issues

--- backend/api/models/serial.py
+++ backend/api/models/serial.py
@@ -18,7 +18,6 @@ class Serial(ArangoDocument):
   quantity: int = 1
   created: datetime | None = Field(default_factory=timestamp)
   released: datetime | None = None
-  available: bool = True
   data: list[SerialFormFieldValue] | None = None
   deleted: bool = False
 
