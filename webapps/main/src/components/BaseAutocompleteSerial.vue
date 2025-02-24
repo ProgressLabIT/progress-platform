316960f96badc85b4d6c1531fdf555a4eb26e18c
Luca Sorgiacomo <luca@progresslab.it>
2025-02-24 15:23:45 +0100

    refactor: BaseAutocompleteSerial to script setup

--- webapps/main/src/components/BaseAutocompleteSerial.vue
+++ webapps/main/src/components/BaseAutocompleteSerial.vue
@@ -106,20 +106,15 @@
   </q-select>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, watch, useTemplateRef } from 'vue';
+import { api } from '@/boot/axios';
 import SerialForm from 'app/src/components/traceability/SerialForm.vue';
 
-export default {
-  name: 'BaseAutocompleteSerial',
-
-  components: {
-    SerialForm,
-  },
-
-  props: {
-    value: {
-      type: [Object, String],
-      default: null,
+const props = defineProps({
+  value: {
+    type: [Object, String],
+    default: null,
     },
 
     label: {
@@ -199,163 +194,126 @@ export default {
 
     minChars: {
       type: Number,
-      default: 0,
-    },
+    default: 0,
   },
+});
 
-  emits: ['select'],
+defineEmits(['select']);
+const selectRef = useTemplateRef('selectRef')
 
-  data() {
-    return {
-      loading: false,
-      create_serial_form: false,
-      options: [],
-      origin_list: [],
-      last_research: undefined,
-      events: NaN,
-      code_free: false, // Serial code for product is taken (false) or not (true)
-    };
-  },
 
-  computed: {
-    placeholder_computed() {
-      return this.value ? null : this.placeholder;
-    },
-    session_data() {
-      return this.$store.state.session;
-    },
-    multiple() {
-      return this.selection_qt != null && this.selection_qt > 1;
-    },
-  },
+const loading = ref(false);
+const create_serial_form = ref(false);
+const options = ref([]);
+const last_research = ref(undefined);
+const code_free = ref(false); // Serial code for product is taken (false) or not (true)
 
-  watch: {
-    initial_values: {
-      handler() {
-        this.initialize();
-      },
-    },
-  },
+const placeholder_computed = computed(() => {
+  return props.value ? null : props.placeholder;
+});
 
-  created() {
-    this.initialize();
-  },
+const multiple = computed(() => {
+  return props.selection_qt != null && props.selection_qt > 1;
+});
 
-  beforeUnmount() {
-    if (this.events) {
-      this.events.close();
-    }
-  },
+watch(props.initial_values, () => {
+  initialize();
+});
 
-  methods: {
-    initialize() {
-      this.create_serial_form = false;
-      if (this.work_order || this.product_key) {
-        this.loadSerials();
-        this.last_research = '';
-      }
-    },
+initialize();
 
-    makeInputUppercase(value) {
-      this.$refs.selectRef.updateInputValue(value.toUpperCase());
-    },
+function initialize() {
+  create_serial_form.value = false;
+  if (props.work_order_key || props.product_key) {
+    loadSerials();
+    last_research.value = '';
+  }
+};
 
-    loadSerials(search_value) {
-      this.loading = true;
-      this.code_free = false;
-      let params = {};
-      if (this.work_order_key) {
-        params.wo_key = this.work_order_key;
-      }
+function makeInputUppercase(value) {
+  selectRef.value.updateInputValue(value.toUpperCase());
+};
 
-      if (this.product_key) {
-        params.product_key = this.product_key;
-      }
+function loadSerials(search_value) {
+  loading.value = true;
+  code_free.value = false;
+  let params = {
+    wo_key: props.work_order_key,
+    product_key: props.product_key,
+    batch_key: props.batch_key,
+    filter_used: props.filter_used,
+    search: search_value,
+    limit: 100,
+  };
 
-      if (this.batch_key) {
-        params.batch_key = this.batch_key;
-      }
-
-      if (this.filter_used) {
-        params.filter_used = this.filter_used;
-      }
-
-      if (search_value) {
-        params.search = search_value;
-        this.last_research = search_value;
-      }
-      params.limit = 100;
-
-      if (search_value) {
-        this.$api
-          .get('serial-code', {
-            params: {
-              serial_code: search_value,
-              product_key: this.product_key,
-            },
-          })
-          .then((resp) => {
-            if (resp.data?.length <= 0) {
-              this.code_free = true;
-            }
-            this.$api.get('serial-selection', { params }).then((resp) => {
-              this.options = resp.data;
-              this.addInitialValues(search_value);
-              this.loading = false;
-            });
-          });
-      } else {
-        this.$api.get('serial-selection', { params }).then((resp) => {
-          this.options = resp.data;
-          this.addInitialValues(search_value);
-          this.loading = false;
-        });
-      }
-    },
-
-    closeCreateForm() {
-      this.create_serial_form = false;
-      this.loadSerials(this.last_research);
-    },
-
-    addInitialValues(search_value) {
-      if (this.initial_values) {
-        if (Array.isArray(this.initial_values)) {
-          for (const serial of this.initial_values) {
-            this.addValue(search_value, serial);
-          }
-        } else {
-          this.addValue(search_value, this.initial_values);
+  if (search_value) {
+    api
+      .get('serial-code', {
+        params: {
+          serial_code: search_value,
+          product_key: props.product_key,
+        },
+      })
+      .then((resp) => {
+        if (resp.data?.length <= 0) {
+          code_free.value = true;
         }
-      }
-    },
-
-    addValue(search_value, serial) {
-      if (
-        (!search_value ||
-          search_value === '' ||
-          serial.label.includes(search_value)) &&
-        this.options.filter((value) => value._key == serial._key).length === 0
-      ) {
-        this.options.push(serial);
-      }
-    },
-
-    filter(value, update, abort) {
-      if (this.last_research === value) {
-        update();
-      } else if (!this.can_search && this.initial_values) {
-        update(() => {
-          this.addInitialValues(value);
+        api.get('serial-selection', { params }).then((resp) => {
+          options.value = resp.data;
+          addInitialValues(search_value);
+          loading.value = false;
         });
-      } else if (value.length < this.minChars && !this.product_key) {
-        abort();
-      } else {
-        update(() => {
-          this.loadSerials(value);
-        });
+      });
+  } else {
+    api.get('serial-selection', { params }).then((resp) => {
+      options.value = resp.data;
+      addInitialValues(search_value);
+      loading.value = false;
+    });
+  }
+};
+
+function closeCreateForm() {
+  create_serial_form.value = false;
+  loadSerials(last_research.value);
+};
+
+function addInitialValues(search_value) {
+  if (props.initial_values) {
+    if (Array.isArray(props.initial_values)) {
+      for (const serial of props.initial_values) {
+        addValue(search_value, serial);
       }
-    },
-  },
+    } else {
+      addValue(search_value, props.initial_values);
+    }
+  }
+};
+
+function addValue(search_value, serial) {
+  if (
+    (!search_value ||
+      search_value === '' ||
+      serial.label.includes(search_value)) &&
+    options.value.filter((value) => value._key == serial._key).length === 0
+  ) {
+    options.value.push(serial);
+  }
+};
+
+function filter(value, update, abort) {
+  if (last_research.value === value) {
+    update();
+  } else if (!props.can_search && props.initial_values) {
+    update(() => {
+      addInitialValues(value);
+    });
+  } else if (value.length < props.minChars && !props.product_key) {
+    abort();
+  } else {
+    update(() => {
+      loadSerials(value);
+    });
+  }
 };
 </script>
