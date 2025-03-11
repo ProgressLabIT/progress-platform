674f56078a052d9afaeaf817ab77969a0025ae96
Luca Sorgiacomo <luca@progresslab.it>
2025-03-11 18:10:39 +0100

    refactor: ProgressOverrideRequested

--- backend/api/events/admin/progress_override_requested.py
+++ backend/api/events/admin/progress_override_requested.py
@@ -1,5 +1,6 @@
 from events.admin.base_admin import BaseAdmin, Queries
 from utils.dt import timestamp
+from models.event import EventInfoModel, EventType
 from models.production import Job, WorkStatus
 from models.traceability import WIP, Batch, WorkSession
 from utils.exceptions import JobHasNoAssigneeError, JobIsActiveError, JobHasActiveBatchError, QuantityOverrideForSerialsNotAllowed, WipNotAvailableError
@@ -10,14 +11,20 @@ from utils.traceability import Queries as TraceabilityQueries
 
 class ProgressOverrideRequested(BaseAdmin):
 
-  @classmethod
-  def get_write_collections(self):
-    return list(set(super().get_write_collections() + ['Batch', 'WorkSession', 'wip', 'Job']))
+  class InfoModel(EventInfoModel):
+    job_key: str
+    new_job_qt_completed: int
+    should_adjust_duration: bool | None = True
+    quantity_change: int | None = None
+
 
   @classmethod
-  def get_read_collections(self):
-    return list(set(super().get_read_collections() + ['Job', 'WorkSession', 'User']))
+  def get_tx_collections(self):
+    return ['Batch', 'WorkOrder','WorkSession', 'wip', 'Job']
 
+  @classmethod
+  def get_event_type(self):
+    return EventType.PROGRESS_OVERRIDE_REQUESTED
 
   def apply(self):
     # TODO: handle StepExecutionData
@@ -35,12 +42,11 @@ class ProgressOverrideRequested(BaseAdmin):
     if self.job.active_batch_qt:
       raise JobHasActiveBatchError("You can't override progress if the job has an active batch. Cancel the current batch first.")
 
-    if self.job.traceability_level:
-      raise QuantityOverrideForSerialsNotAllowed("You can't override progress with traceability enabled, you can reset the job instead.")
+    if self.job.traceability_level and self.info.new_job_qt_completed > self.job.qt_completed:
+      raise QuantityOverrideForSerialsNotAllowed("You can't increase progress with traceability enabled.")
 
     # Initialize job update.
     # Will save at the end after enrichment based on override type
-
     new_job_progress = round(100 * self.info.new_job_qt_completed / self.job.qt_planned)
 
     job_update = dict(
@@ -48,11 +54,11 @@ class ProgressOverrideRequested(BaseAdmin):
       qt_completed = self.info.new_job_qt_completed,
       qt_released = self.info.new_job_qt_completed,
       progress = new_job_progress,
-      forced = self.id
+      forced = self.event_key
     )
 
     # Get available wip data
-    bind_vars = dict(job_key=self.job_key)
+    bind_vars = dict(job_key=self.info.job_key)
     self.wip = self.tx.aql.execute(
       TraceabilityQueries.GET_AVAILABLE_WIP_UPSTREAM_AND_DOWNSTREAM_OF_JOB,
       bind_vars = bind_vars
@@ -62,13 +68,13 @@ class ProgressOverrideRequested(BaseAdmin):
     free_wip_qt_downstream = sum(w['quantity'] for w in self.wip['downstream_free_wip'])
 
     # Check quantity delta
-    quantity_update = self.new_job_qt_completed - self.job.qt_completed
+    self.info.quantity_change = self.info.new_job_qt_completed - self.job.qt_completed
 
     # No quantity change
-    if not quantity_update:
+    if not self.info.quantity_change:
       raise ValueError('No quantity change')
 
-    elif self.new_job_qt_completed > self.job.qt_planned:
+    elif self.info.new_job_qt_completed > self.job.qt_planned:
       raise ValueError('Quantity is higher than the total planned')
 
     total_duration = self.tx.aql.execute(
@@ -83,19 +89,19 @@ class ProgressOverrideRequested(BaseAdmin):
     ).next()
 
     # Progress increase: check there's wip available to pick from
-    if quantity_update > 0:
-      if quantity_update > free_wip_qt_upstream and not self.job.first_phase:
+    if self.info.quantity_change > 0:
+      if self.info.quantity_change > free_wip_qt_upstream and not self.job.first_phase:
         raise WipNotAvailableError("The previous phase has not made enough progress to make this change")
 
       # Set job as started if not already
       if self.job.stage == WorkStatus.CREATED:
         job_update['stage'] = WorkStatus.STARTED
-        job_update['start'] = self.timestamp
+        job_update['start'] = self.info.timestamp
 
       # Set job as closed and remove it from queues if necessary
-      if self.new_job_qt_completed == self.job.qt_planned:
+      if self.info.new_job_qt_completed == self.job.qt_planned:
         job_update['stage'] = WorkStatus.CLOSED
-        job_update['end'] = self.timestamp
+        job_update['end'] = self.info.timestamp
         self.tx.aql.execute(
           ProductionQueries.REMOVE_JOB_FROM_QUEUE,
           bind_vars = dict(
@@ -105,15 +111,15 @@ class ProgressOverrideRequested(BaseAdmin):
         )
 
       # 2. Create forced batch, work_session and downstream wip:
-      if self.should_adjust_duration:
-        new_batch_key, batch_value = self._create_forced_traceability_records(quantity_update)
+      if self.info.should_adjust_duration:
+        new_batch_key, batch_value = self._create_forced_traceability_records(self.info.quantity_change)
       else:
         # Cancel all work sessions to recreate them with the new durations
         ws_cursor = self.tx.aql.execute(
           Queries.CANCEL_JOB_WORK_SESSIONS,
           bind_vars = dict(
             job_key = self.job.key,
-            event_id = self.id
+            event_key = self.event_key
           )
         )
         canceled_work_sessions = [WorkSession(**ws) for ws in ws_cursor]
@@ -139,7 +145,7 @@ class ProgressOverrideRequested(BaseAdmin):
             product_key = self.job.product_key,
             hourly_cost = average_hourly_cost,
             duration = int(total_duration * quantity_ratio),
-            forced = self.id
+            forced = self.event_key
           )
           new_work_sessions.append(
             work_session.dict(exclude={'key', 'id', 'rev'})
@@ -148,15 +154,15 @@ class ProgressOverrideRequested(BaseAdmin):
         self.tx.collection('WorkSession').insert_many(new_work_sessions)
 
         # For the increased quantity, create a new batch/work session/wip, use the leftover duration
-        quantity_ratio = quantity_update / self.new_job_qt_completed
+        quantity_ratio = self.info.quantity_change / self.new_job_qt_completed
         new_batch_key, batch_value = self._create_forced_traceability_records(
-          quantity=quantity_update,
+          quantity=self.info.quantity_change,
           duration=total_duration * quantity_ratio
         )
 
       # 3. Delete free wip from previous phase to current (if not first phase)
       if not self.job.first_phase:
-        remaining_wip_to_remove = abs(quantity_update)
+        remaining_wip_to_remove = abs(self.info.quantity_change)
         wip_records = deque(self.wip['upstream_free_wip'])
         records_to_delete = deque()
 
@@ -176,7 +182,7 @@ class ProgressOverrideRequested(BaseAdmin):
         self.tx.collection('wip').delete_many(records_to_delete)
 
     else: # quantity_update < 0, Progress decrease: check enough wip downstream has not already been booked/used
-      if abs(quantity_update) > free_wip_qt_downstream and not self.job.last_phase:
+      if abs(self.info.quantity_change) > free_wip_qt_downstream and not self.job.last_phase:
         raise WipNotAvailableError("You can't reduce the released quantity of this phase below that already completed/started/booked from the following phase")
 
       # Reopen job and restore it into queue if closed
@@ -185,7 +191,7 @@ class ProgressOverrideRequested(BaseAdmin):
         job_update['end'] = None
 
       # Reset job to created status if necessary
-      if self.new_job_qt_completed == 0:
+      if self.info.new_job_qt_completed == 0:
         job_update['stage'] = WorkStatus.CREATED
 
       # 2. Flag last N batches with `canceled: true`
@@ -194,14 +200,14 @@ class ProgressOverrideRequested(BaseAdmin):
         bind_vars=dict(job_key=self.job.key)
       )
       job_batches = deque(Batch(**b) for b in job_batches_cursor)
-      remaining_qt_to_remove = abs(quantity_update)
+      remaining_qt_to_remove = abs(self.info.quantity_change)
       batches_to_cancel = deque()
 
       # Here we'll cancel only full batches.
       # remaining_qt_to_remove can become negative, will compensate later on
       while remaining_qt_to_remove > 0:
         current_batch = job_batches.popleft()
-        current_batch.canceled = self.id
+        current_batch.canceled = self.event_key
         batches_to_cancel.append(current_batch)
         remaining_qt_to_remove -= current_batch.qt_pass
 
@@ -214,17 +220,17 @@ class ProgressOverrideRequested(BaseAdmin):
       self.tx.collection('Batch').update_many(batch_updates)
       canceled_batches_keys = [b.key for b in batches_to_cancel]
 
-      if self.should_adjust_duration:
+      if self.info.should_adjust_duration:
         # 3. Flag the work sessions of the canceled batches with `canceled: true`
         self.tx.aql.execute(
           """
           FOR ws IN WorkSession
           FILTER ws.batch_key IN @canceled_batches_keys
-          UPDATE ws WITH { canceled: @event_id } in WorkSession
+          UPDATE ws WITH { canceled: @event_key } in WorkSession
           """,
           bind_vars = dict(
             canceled_batches_keys = canceled_batches_keys,
-            event_id = self.id
+            event_key = self.event_key
           )
         )
       else:
@@ -233,7 +239,7 @@ class ProgressOverrideRequested(BaseAdmin):
           Queries.CANCEL_JOB_WORK_SESSIONS,
           bind_vars = dict(
             job_key = self.job.key,
-            event_id = self.id
+            event_key = self.event_key
           )
         )
         canceled_work_sessions = [WorkSession(**ws) for ws in ws_cursor]
@@ -252,7 +258,7 @@ class ProgressOverrideRequested(BaseAdmin):
         average_hourly_cost = 0 if not canceled_work_sessions else sum(ws.hourly_cost or 0 for ws in canceled_work_sessions) / len(canceled_work_sessions) * work_session_amount_ratio
         new_work_sessions = []
         for batch in job_batches:
-          quantity_ratio = batch.qt_pass / self.new_job_qt_completed
+          quantity_ratio = batch.qt_pass / self.info.new_job_qt_completed
           work_session = WorkSession(
             batch_key = batch.key,
             job_key = self.job.key,
@@ -261,7 +267,7 @@ class ProgressOverrideRequested(BaseAdmin):
             product_key = self.job.product_key,
             hourly_cost = average_hourly_cost,
             duration = total_duration * quantity_ratio,
-            forced = self.id
+            forced = self.event_key
           )
           new_work_sessions.append(work_session.dict(exclude={'key', 'id', 'rev'}))
 
@@ -285,11 +291,11 @@ class ProgressOverrideRequested(BaseAdmin):
 
       # 5. If we canceled more than required, create a forced batch/work session/wip to compensate
       if remaining_qt_to_remove < 0:
-        quantity_ratio = abs(remaining_qt_to_remove) / self.new_job_qt_completed
+        quantity_ratio = abs(remaining_qt_to_remove) / self.info.new_job_qt_completed
         duration_to_preserve_total = total_duration * quantity_ratio
         new_batch_key, batch_value = self._create_forced_traceability_records(
           quantity=abs(remaining_qt_to_remove),
-          duration=duration_to_preserve_total if not self.should_adjust_duration else None,
+          duration=duration_to_preserve_total if not self.info.should_adjust_duration else None,
           unit_processing_time=avg_unit_processing_time
         )
 
@@ -328,7 +334,7 @@ class ProgressOverrideRequested(BaseAdmin):
           )
         )
         batches_from_previous_phase = deque(Batch(**b) for b in cursor)
-        wip_to_add = abs(quantity_update)
+        wip_to_add = abs(self.info.quantity_change)
 
         while wip_to_add:
           wip_batch = batches_from_previous_phase.popleft()
@@ -351,7 +357,7 @@ class ProgressOverrideRequested(BaseAdmin):
 
     self.tx.collection('Job').update(job_update)
 
-    if self.job.stage == WorkStatus.CLOSED and quantity_update < 0:
+    if self.job.stage == WorkStatus.CLOSED and self.info.quantity_change < 0:
       self.tx.aql.execute(ProductionQueries.ADD_JOB_TO_QUEUE,
         bind_vars = dict(
           job_key = self.job.key,
@@ -431,12 +437,12 @@ class ProgressOverrideRequested(BaseAdmin):
       work_order_key = self.job.wo_key,
       phase_key = self.job.phase_key,
       active = False,
-      start = self.timestamp,
-      end = self.timestamp,
+      start = self.info.timestamp,
+      end = self.info.timestamp,
       qt_pass = quantity,
       qt_total = quantity,
       value = batch_value,
-      forced = self.id
+      forced = self.event_key
     )
 
     new_batch_key = self.tx.collection('Batch').insert(new_batch_data)['_key']
@@ -449,7 +455,7 @@ class ProgressOverrideRequested(BaseAdmin):
       product_key = self.job.product_key,
       hourly_cost = hourly_cost,
       duration = duration or (unit_processing_time * quantity),
-      forced = self.id
+      forced = self.event_key
     )
 
     self.tx.collection('WorkSession').insert(new_work_sessions_data)
