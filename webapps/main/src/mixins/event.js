7df8a00761b48b8d411b410ce83f409d4bb7a454
Luca Sorgiacomo <luca@progresslab.it>
2025-03-15 11:21:28 +0100

    fix: show event errors in the client

--- webapps/main/src/mixins/event.js
+++ webapps/main/src/mixins/event.js
@@ -12,7 +12,18 @@ export default {
           timestamp: timestamp(),
           ...event_data,
         };
-        this.$api.post('event', event).then((resp) => resolve(resp));
+        this.$api.post('event', event)
+          .then((resp) => resolve(resp))
+          .catch((err) => {
+            this.$q.notify({
+              message: err.response.data.detail.message,
+              color: 'theme-red',
+              timeout: 0,
+              position: 'top',
+              actions: [{ label: 'CLOSE', color: 'white', handler: () => {} }],
+            });
+            throw err;
+          });
       });
     },
   },
