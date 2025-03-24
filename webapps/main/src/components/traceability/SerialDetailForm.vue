9dc89d1f5b9fe295815e75c10cbc1840e863a2bd
Luca Sorgiacomo <luca@progresslab.it>
2025-03-24 07:05:03 +0100

    fix: use inventory to show availability in SerialDetailForm

--- webapps/main/src/components/traceability/SerialDetailForm.vue
+++ webapps/main/src/components/traceability/SerialDetailForm.vue
@@ -182,6 +182,7 @@ export default {
       context: 'serial',
       contextData: props.serial_key,
     });
+
     return {
       config,
       openPrintDialog,
@@ -200,6 +201,7 @@ export default {
       current_step: 0,
       data_column_width: 65,
       events: NaN,
+      serialAvailable: false,
     };
   },
 
@@ -207,7 +209,7 @@ export default {
     serial_key: {
       immediate: true,
       handler() {
-        this.getHistory();
+        this.getInfo();
       },
     },
   },
@@ -228,10 +230,6 @@ export default {
       return this.$capitalize(
         this.$formatDateTime(this.serial?.created, this.$i18n.locale, config),
       );
-    },
-
-    serialAvailable() {
-      return [true, false].includes(this.serial.available) ? this.serial.available : true
     },
 
     form_fields() {
@@ -260,7 +258,7 @@ export default {
 
   created() {
     this.$store.dispatch('loadUsers');
-    this.getHistory();
+    this.getInfo();
     let eventURL =
       this.$api.defaults.baseURL + '/notification/serial-notification';
     this.events = new EventSource(eventURL, {
@@ -278,16 +276,26 @@ export default {
   },
 
   methods: {
-    getHistory() {
+    getInfo() {
+      // Get history
       this.$api
         .get('event', { params: { serial_key: this.serial_key } })
         .then((resp) => (this.history = resp.data));
+
+      // Get inventory availability
+      this.$api.get('inventory', { params: { serial_key: this.serial_key } })
+      .then((resp) => {
+        this.serialAvailable = resp.data.length > 0
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+      });
     },
 
     handleMessage(message) {
       let event = JSON.parse(message.data);
       if (event?.serial_key === this.serial_key) {
-        this.getHistory();
+        this.getInfo();
       }
     },
 
