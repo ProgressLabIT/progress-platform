b18485c3bf3285f83d2e39fe59e2c09e7dd5d85c
Luca Sorgiacomo <luca@progresslab.it>
2025-03-31 10:25:40 +0200

    fix: batch_serials default in BatchCreated
    avoids "None has no len" issue when checking for length of the property

--- backend/api/events/production/batch_created.py
+++ backend/api/events/production/batch_created.py
@@ -71,7 +71,7 @@ class BatchCreatedEvent(BaseProductionEvent):
       product = self.tx.collection('Product').get(self.info.product_key)
       start_with_code = product.get('serialcode_on_batchstart', False)
       counter_key = product.get('counter_key', None)
-      if start_with_code and not len(self.info.batch_serials) and counter_key is None:
+      if start_with_code and (self.info.batch_serials is None or len(self.info.batch_serials) == 0) and counter_key is None:
         raise ValueError("You must provide serial codes or define counter to start a new batch for this product")
 
       self._create_serial_records(
