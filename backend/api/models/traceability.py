480b3c13f40af915320e6673156f7250b9cd982d
Luca Sorgiacomo <luca@progresslab.it>
2025-04-18 14:32:19 +0200

    fix: allow no batch key for wip
    necessary for overrides restoring wip in the previous phase. It doesn't
    feel worth it, or even possible, to figure out to which batch from
    previous jobs the wip should be linked in this case.

--- backend/api/models/traceability.py
+++ backend/api/models/traceability.py
@@ -91,7 +91,7 @@ class WorkSession(FlexModel):
 
 class WIP(ArangoEdge):
   # _from & _to refer to process phases or specific jobs
-  batch_key: str # batch that produced the wip
+  batch_key: str | None = None # batch that produced the wip
   wo_key: str
   product_key: str
   serial_key: str | None = None
