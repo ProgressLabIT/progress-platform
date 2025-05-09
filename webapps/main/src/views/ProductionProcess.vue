ProgressLabIT/progress-platform

commit b079343a41470e8b96e1710ffa7a30bd1d9bf94f

feat: prevent deleteing phase with components in the client

webapps/main/src/views/ProductionProcess.vue
@@ -78,10 +78,10 @@
                   @icon-click="update_alias_at_index = index"
                 />
                 <BaseTooltipIcon
-                  icon="mdi-delete"
-                  :tooltip="$t('delete')"
-                  :color="$theme.red"
-                  @icon-click="confirming_delete = index"
+                  :icon="phase.has_components ? 'mdi-delete-off-outline' : 'mdi-delete'"
+                  :tooltip="$t(phase.has_components ? 'phase_delete_has_components' : 'delete')"
+                  :color="phase.has_components ? 'theme-grey' : $theme.red"
+                  @icon-click="deleteClick(index)"
                 />
               </div>
             </q-item-section>
@@ -339,7 +339,13 @@ export default {
 
     process: {
       get() {
-        return this.$store.state.process.temp;
+        const bom = this.$store.state.bom.saved
+        return this.$store.state.process.temp.map(p => {
+          return {
+            ...p,
+            has_components: bom.some(b => b.phase_key === p._key),
+          }
+        });
       },
 
       set(value) {
@@ -468,6 +474,13 @@ export default {
         print_templates: [],
       });
       this.current_phase = this.process.length - 1;
+    },
+
+    deleteClick(index) {
+      if (this.process[index].has_components) {
+        return;
+      }
+      this.confirming_delete = index;
     },
 
     deletePhase(phase_index) {

webapps/main/src/i18n/en.js
@@ -535,6 +535,7 @@ export default {
       instruction: 'instructions',
     },
   },
+  phase_delete_has_components: 'Phase has components, can\'t be deleted',
   preferences: {
     theme: {
       light: 'Light',

webapps/main/src/i18n/it.js
@@ -1083,4 +1083,5 @@ export default {
     output_position: 'Posizione di versamento',
   },
   release_date: 'data rilascio',
+  phase_delete_has_components: 'La fase ha componenti, non può essere eliminata',
 };

commit fa5ead6247508334549a7d6039ed154d61eafadc

fix: assign process explicitly to trigger reactivity
adding the bom mapping to the process in b079343a broke reactivity

webapps/main/src/views/ProductionProcess.vue
@@ -462,7 +462,7 @@ export default {
         })) ?? [],
       );
 
-      this.process.push({
+      this.process = [...this.process, {
         // Add temp _key so that sorting works with new phases too
         _key: Date.now(),
         alias: new_operation.name,
@@ -472,7 +472,7 @@ export default {
         production_notes: new_operation.default_phase_notes,
         steps,
         print_templates: [],
-      });
+      }];
       this.current_phase = this.process.length - 1;
     },
 