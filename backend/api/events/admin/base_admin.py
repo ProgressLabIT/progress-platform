117364ba78f326e077620db3a7fdf1586417b458
Luca Sorgiacomo <luca@progresslab.it>
2025-05-31 23:25:43 +0200

    refactor: reduce wip method into BaseAdmin for reuse

--- backend/api/events/admin/base_admin.py
+++ backend/api/events/admin/base_admin.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Any
 
 from events.production.base_production import BaseProductionEvent
@@ -38,3 +39,24 @@ class BaseAdmin(BaseProductionEvent):
   def flag_job_as_forced(self):
     job_update = dict(_key=self.info.job_key, forced=self.event_key)
     self.tx.collection('Job').update(job_update)
+
+
+  def _reduce_wip(self, wip_records, quantity):
+    remaining_wip_to_remove = quantity
+    wip_records = deque(wip_records)
+    records_to_delete = deque()
+
+    while remaining_wip_to_remove:
+      current_wip = wip_records.popleft()
+
+      if current_wip['quantity'] <= remaining_wip_to_remove:
+        records_to_delete.append(current_wip)
+        remaining_wip_to_remove -= current_wip['quantity']
+      else:
+        leftover_wip = current_wip['quantity'] - remaining_wip_to_remove
+        new_value = round(current_wip['value'] * leftover_wip / current_wip['quantity'], 4)
+        wip_update = dict(_key=current_wip['_key'], quantity=leftover_wip, value=new_value)
+        self.tx.collection('wip').update(wip_update)
+        remaining_wip_to_remove = 0
+
+    self.tx.collection('wip').delete_many(records_to_delete)
--- backend/api/events/admin/progress_override_requested.py
+++ backend/api/events/admin/progress_override_requested.py
@@ -203,6 +203,7 @@ class ProgressOverrideRequestedEvent(BaseAdmin):
 
     # Reduce upstream WIP
     if not self.job.first_phase:
+      # _reduce_wip is inherited from BaseAdmin
       self._reduce_wip(self.wip['upstream_free_wip'], self.info.quantity_change)
 
     if self.handle_inventory:
@@ -502,29 +503,6 @@ class ProgressOverrideRequestedEvent(BaseAdmin):
         new_session_duration = self._handle_time_redistribution()
 
     return new_session_duration
-
-  # =================================================================================================
-
-  def _reduce_wip(self, wip_records, quantity):
-    remaining_wip_to_remove = quantity
-    wip_records = deque(wip_records)
-    records_to_delete = deque()
-
-    while remaining_wip_to_remove:
-      current_wip = wip_records.popleft()
-
-      if current_wip['quantity'] <= remaining_wip_to_remove:
-        records_to_delete.append(current_wip)
-        remaining_wip_to_remove -= current_wip['quantity']
-      else:
-        leftover_wip = current_wip['quantity'] - remaining_wip_to_remove
-        new_value = round(current_wip['value'] * leftover_wip / current_wip['quantity'], 4)
-        wip_update = dict(_key=current_wip['_key'], quantity=leftover_wip, value=new_value)
-        self.tx.collection('wip').update(wip_update)
-        remaining_wip_to_remove = 0
-
-    self.tx.collection('wip').delete_many(records_to_delete)
-
 
 
   # =================================================================================================
