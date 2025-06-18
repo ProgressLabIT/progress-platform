856dd9414bd28c5c2c202191cadc798e57e500fb
Luca Sorgiacomo <luca@progresslab.it>
2025-06-18 17:00:34 +0200

    fix: restore deleted default in serial model
    created regression in FIND_WO_SERIALS query. Instead of correcting
    the query, reset default as false to allow query to use equality check
    which can leverage indexes

--- backend/api/models/serial.py
+++ backend/api/models/serial.py
@@ -19,7 +19,7 @@ class Serial(ArangoDocument):
   created: datetime | None = Field(default_factory=timestamp)
   released: datetime | None = None
   data: list[SerialFormFieldValue] | None = None
-  deleted: str | bool | None = None # event key of the deletion event if deleted, null or false if not deleted
+  deleted: str | bool | None = False # event key of the deletion event if deleted, false if not deleted
 
 class SerialSelection(BaseModel):
    serial_key: str | None = Field(None, validation_alias='_key')
