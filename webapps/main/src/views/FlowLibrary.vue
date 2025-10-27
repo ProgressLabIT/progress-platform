06bcf53bc6814e971ce7ef7d3f51257f2c9c39c4
Luca Sorgiacomo <luca@progresslab.it>
2025-10-27 16:46:32 +0100

    fix: revert reporting and workflow to using ports
    subdomain configuration not working for now (see comments to #769

--- webapps/main/src/views/FlowLibrary.vue
+++ webapps/main/src/views/FlowLibrary.vue
@@ -129,7 +129,7 @@ export default {
 
   computed: {
     base_url() {
-      return window.location.protocol + '//' + window.location.hostname + '/wf-api';
+      return window.location.protocol + '//' + window.location.hostname + ':4200';
     },
 
     deployments_url() {
--- webapps/main/src/views/ReportRoot.vue
+++ webapps/main/src/views/ReportRoot.vue
@@ -16,7 +16,7 @@ const src = computed(() => {
   const sessionData = store.state.session;
   const sessionDataStr = JSON.stringify(sessionData);
   const encodedSession = btoa(sessionDataStr);
-  return `${protocol}//reporting.${domain}?session=${encodedSession}`;
+  return `${protocol}//${domain}:8501?session=${encodedSession}`;
 });
 
 
